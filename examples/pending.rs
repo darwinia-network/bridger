662cdff12f30d27d0dd4ba259442a64a76e61d7e
clearloop <udtrokia@gmail.com>
2020-10-09 18:10:28 +0800

    feat(trans): implement transfer example using node-template runtime

---
+++ examples/pending.rs
@@ -0,0 +1,24 @@
+use darwinia_bridger::Config;
+use sp_keyring::{sr25519::sr25519::Pair, AccountKeyring};
+use substrate_subxt::{
+    balances::*, sp_core::Pair as PairTrait, ClientBuilder, NodeTemplateRuntime, PairSigner,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config::new(None).unwrap();
+    env_logger::init();
+
+    let pair = Pair::from_string(&config.seed, None).unwrap();
+    let signer = PairSigner::new(pair);
+    let dest = AccountKeyring::Bob.to_account_id().into();
+
+    let client = ClientBuilder::<NodeTemplateRuntime>::new()
+        .set_url(config.node)
+        .build()
+        .await?;
+    let hash = client.transfer(&signer, &dest, 10_000).await?;
+
+    println!("Balance transfer extrinsic submitted: {:?}", hash);
+    Ok(())
+}

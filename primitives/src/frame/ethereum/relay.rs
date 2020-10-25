1f487bb6577c5bff15745057934d09c8dc67a56c
clearloop <udtrokia@gmail.com>
2020-10-25 22:20:33 +0800

    feat(subscribe): add events of Ethereum Relay

--- primitives/src/frame/ethereum/relay.rs
+++ primitives/src/frame/ethereum/relay.rs
@@ -1,13 +1,19 @@
 //! Ethereum Relay
-use crate::chain::ethereum::{EthereumRelayHeaderParcel, EthereumRelayProofs};
-use codec::Encode;
+use crate::chain::ethereum::{
+    EthereumHeader, EthereumReceipt, EthereumRelayHeaderParcel, EthereumRelayProofs,
+};
+use codec::{Decode, Encode};
 use core::marker::PhantomData;
 use substrate_subxt::system::{System, SystemEventsDecoder};
-use substrate_subxt_proc_macro::{module, Call, Store};
+use substrate_subxt_proc_macro::{module, Call, Event, Store};
 
 /// Ethereum Relay Pallet
 #[module]
 pub trait EthereumRelay: System {}
+
+//////
+// Call
+//////
 
 /// Affirm Call
 #[derive(Clone, Debug, PartialEq, Call, Encode)]
@@ -29,14 +35,6 @@ pub struct SetConfirmedParcel<T: EthereumRelay> {
     pub _runtime: PhantomData<T>,
 }
 
-/// PendingHeaders Storage
-#[derive(Clone, Debug, Eq, PartialEq, Store, Encode)]
-pub struct ConfirmedBlockNumbers<T: EthereumRelay> {
-    #[store(returns = Vec<u64>)]
-    /// Runtime marker
-    pub _runtime: PhantomData<T>,
-}
-
 /// Approve pending header call
 #[derive(Clone, Debug, PartialEq, Call, Encode)]
 pub struct ApprovePendingRelayHeaderParcel<T: EthereumRelay> {
@@ -49,8 +47,45 @@ pub struct ApprovePendingRelayHeaderParcel<T: EthereumRelay> {
 /// Reject pending header call
 #[derive(Clone, Debug, PartialEq, Call, Encode)]
 pub struct RejectPendingRelayHeaderParcel<T: EthereumRelay> {
-    /// pending block number
+    /// Pending block number
     pub pending: u64,
+    /// Runtime marker
+    pub _runtime: PhantomData<T>,
+}
+
+//////
+// Event
+//////
+/// Remove confirmed parcel
+#[derive(Clone, Debug, PartialEq, Event, Decode)]
+pub struct RemoveConfirmedParcel<T: EthereumRelay> {
+    /// The block number of Ethereum header parcel
+    pub block: u64,
+    /// Runtime marker
+    pub _runtime: PhantomData<T>,
+}
+
+/// Remove confirmed parcel
+#[derive(Clone, Debug, PartialEq, Event, Decode)]
+pub struct VerifyReceipt<T: EthereumRelay> {
+    /// The block number of Ethereum header parcel
+    pub account_id: <T as System>::AccountId,
+    /// Ethereum Receipt
+    pub receipt: EthereumReceipt,
+    /// Ethereum Header
+    pub header: EthereumHeader,
+    /// Runtime marker
+    pub _runtime: PhantomData<T>,
+}
+
+//////
+// Store
+//////
+
+/// PendingHeaders Storage
+#[derive(Clone, Debug, Eq, PartialEq, Store, Encode)]
+pub struct ConfirmedBlockNumbers<T: EthereumRelay> {
+    #[store(returns = Vec<u64>)]
     /// Runtime marker
     pub _runtime: PhantomData<T>,
 }

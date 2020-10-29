f11a588ce241575dbb5e10f734f9f2183daff07d
clearloop <udtrokia@gmail.com>
2020-10-29 22:39:47 +0800

    perf(entry): optimize the output of the top-level unwrap

--- src/bin/bridger.rs
+++ src/bin/bridger.rs
@@ -2,7 +2,7 @@ use darwinia_bridger::cmd;
 
 #[tokio::main]
 async fn main() {
-    if let Ok(err) = cmd::exec().await {
+    if let Err(err) = cmd::exec().await {
         log::error!("{:?}", err);
     }
 }

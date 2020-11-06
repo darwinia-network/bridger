4934dcf19a8209e0fb4bc010651c9a80627cdb8c
Xavier Lau <c.estlavie@icloud.com>
2020-11-07 02:36:27 +0800

    handle: error

--- src/service/guard.rs
+++ src/service/guard.rs
@@ -2,9 +2,9 @@
 use crate::{
     api::{Darwinia, Shadow},
     config::Config,
-    result::{Result as BridgerResult, Error::Bridger},
-    service::Service,
     memcache::MemCache,
+    result::{Error::Bridger, Result as BridgerResult},
+    service::Service,
 };
 use async_trait::async_trait;
 use std::{
@@ -14,6 +14,19 @@ use std::{
 
 /// Attributes
 const SERVICE_NAME: &str = "GUARD";
+
+macro_rules! quick_ok {
+    ($expr:expr, $err:expr) => {{
+        match $expr.await {
+            Ok(v) => v,
+            Err(e) => {
+                error!("{}, due to `{}`", $err, e);
+
+                continue;
+            }
+        }
+    }};
+}
 
 /// Redeem Service
 pub struct GuardService {
@@ -45,31 +58,45 @@ impl Service for GuardService {
         self.role_checking().await?;
 
         loop {
-            let last_confirmed = self.darwinia.last_confirmed().await?;
+            let last_confirmed = quick_ok!(
+                self.darwinia.last_confirmed(),
+                "Failed to get last confirmed"
+            );
             info!("Last confirmed ethereum block number is {}", last_confirmed);
 
             trace!("Checking pending headers...");
-            let pending_headers = self.darwinia.pending_headers().await?;
+            let pending_headers = quick_ok!(
+                self.darwinia.pending_headers(),
+                "Failed to get pending headers"
+            );
             for pending in pending_headers {
                 if !self.darwinia.account.has_voted(pending.2) {
                     let pending_parcel = pending.1;
                     let pending_block_number: u64 = pending_parcel.header.number;
-                    let parcel = self.shadow.parcel(pending_block_number as usize).await?;
-
-                    let parcel_fulfilled = !(
-                        parcel.header.hash.is_none()
-                        || parcel.header.hash.unwrap() == [0u8; 32]
-                        || parcel.mmr_root == [0u8; 32]
+                    let parcel = quick_ok!(
+                        self.shadow.parcel(pending_block_number as usize),
+                        "Failed to get pendind parcel"
                     );
+                    let parcel_fulfilled = !(parcel.header.hash.is_none()
+                        || parcel.header.hash.unwrap() == [0u8; 32]
+                        || parcel.mmr_root == [0u8; 32]);
 
                     if parcel_fulfilled {
                         // delay to wait for possible previous extrinsics
                         tokio::time::delay_for(Duration::from_secs(12)).await;
                         if parcel.is_same_as(&pending_parcel) {
-                            self.darwinia.vote_pending_relay_header_parcel(pending_block_number, true).await?;
+                            quick_ok!(
+                                self.darwinia
+                                    .vote_pending_relay_header_parcel(pending_block_number, true),
+                                "Failed to vote for pending parcel"
+                            );
                             info!("Voted to approve {}", pending_block_number);
                         } else {
-                            self.darwinia.vote_pending_relay_header_parcel(pending_block_number, false).await?;
+                            quick_ok!(
+                                self.darwinia
+                                    .vote_pending_relay_header_parcel(pending_block_number, false),
+                                "Failed to vote for pending parcel"
+                            );
                             info!("Voted to reject {}", pending_block_number);
                         };
                     }

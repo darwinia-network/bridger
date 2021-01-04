6a50e5d95679e972f63a8deccdc88ea61e1454b7
Aki <wuminzhe@gmail.com>
2021-01-04 15:07:34 +0800

    Register type size of EthereumTransactionIndex

--- src/api/darwinia.rs
+++ src/api/darwinia.rs
@@ -545,6 +545,7 @@ impl Darwinia {
 		decoder.register_type_size::<EcdsaSignature>("RelayAuthoritySignature");
 		decoder.register_type_size::<u8>("ElectionCompute"); // just a hack
 		decoder.register_type_size::<u32>("Term");
+		decoder.register_type_size::<u64>("EthereumTransactionIndex");
 
 		let raw_events = decoder.decode_events(&mut &storage_data.0[..])?;
 		for (_, raw) in raw_events {

45273208f01f2584565efaba29848fe0124800b6
fewensa <fewensa@protonmail.com>
2021-06-25 19:15:01 +0800

    fix compile bug

--- bridger/bridge-standard/src/bridge/shared_service.rs
+++ bridger/bridge-standard/src/bridge/shared_service.rs
@@ -4,5 +4,7 @@ use crate::bridge::service::BridgeService;
 use crate::bridge::task::BridgeTask;
 
 pub trait SharedService<T: BridgeTask + 'static>: BridgeService<T> + Debug {
-    fn spawn_with_shared(bus: &T::Bus) -> anyhow::Result<Self>;
+    fn spawn_with_shared(bus: &T::Bus) -> anyhow::Result<Self>
+    where
+        Self: Sized;
 }

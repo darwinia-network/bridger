ade0dfa465640ea6dbe4b2c3b47ece56a9238de3
fewensa <fewensa@protonmail.com>
2021-07-03 03:51:20 +0800

    command

--- bridger/bin/src/types/command.rs
+++ bridger/bin/src/types/command.rs
@@ -33,15 +33,15 @@ pub enum TaskCommand {
     List,
     /// Start a task
     Start {
-        /// The task name
-        #[structopt(short, long)]
-        name: String,
-        /// The config format, supports [toml|json|yml]
-        #[structopt(long, default_value = "toml")]
-        format: String,
-        /// The config file path, When first run this is required, but the server already have this task config, can be skip this parameter
-        #[structopt(short, long)]
-        config: Option<PathBuf>,
+        /// Options of task control
+        #[structopt(flatten)]
+        options: TaskControlOptions,
+    },
+    /// Restart a task
+    Restart {
+        /// Options of task control
+        #[structopt(flatten)]
+        options: TaskControlOptions,
     },
     /// Stop a running task
     Stop {
@@ -49,6 +49,19 @@ pub enum TaskCommand {
         #[structopt(short, long)]
         name: String,
     },
+}
+
+#[derive(Clone, Debug, StructOpt)]
+pub struct TaskControlOptions {
+    /// The task name
+    #[structopt(short, long)]
+    pub name: String,
+    /// The config format, supports [toml|json|yml]
+    #[structopt(long, default_value = "toml")]
+    pub format: String,
+    /// The config file path, When first run this is required, but the server already have this task config, can be skip this parameter
+    #[structopt(short, long)]
+    pub config: Option<PathBuf>,
 }
 
 // #[derive(Debug, StructOpt)]

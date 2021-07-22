7dd1032c74756370237be5f57aa266719d318f8e
fewensa <fewensa@protonmail.com>
2021-07-22 17:05:34 +0800

    bridger base path use home_dir first.

--- bin/src/patch/bridger.rs
+++ bin/src/patch/bridger.rs
@@ -2,8 +2,8 @@ use std::path::PathBuf;
 
 pub fn base_path(except_path: Option<PathBuf>) -> anyhow::Result<PathBuf> {
     let base_path = except_path.unwrap_or_else(|| {
-        let mut path = std::env::temp_dir();
-        path.push("darwinia-bridger");
+        let mut path = dirs::home_dir().unwrap_or_else(|| std::env::temp_dir());
+        path.push(".bridger");
         path
     });
     if !base_path.exists() {

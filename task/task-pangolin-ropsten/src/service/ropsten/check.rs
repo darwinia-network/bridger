ffb8c17f4e2563c3feb930bbe43759a34d7b38f2
fewensa <fewensa@protonmail.com>
2021-08-26 10:45:47 +0800

    check

--- task/task-pangolin-ropsten/src/service/ropsten/check.rs
+++ task/task-pangolin-ropsten/src/service/ropsten/check.rs
@@ -1,4 +1,9 @@
+use bridge_traits::bridge::component::BridgeComponent;
+use component_pangolin_subxt::component::DarwiniaSubxtComponent;
+use component_pangolin_subxt::darwinia::client::Darwinia;
 use support_tracker::Tracker;
+
+use crate::task::PangolinRopstenTask;
 
 pub struct RopstenScanChecker {
     tracker: Tracker,
@@ -15,16 +20,46 @@ impl RopstenScanChecker {
         loop {
             if let Err(e) = self.run().await {
                 log::error!("Failed to check redeem: {:?}", e);
+                tokio::time::sleep(std::time::Duration::from_secs(1));
             }
-            tokio::time::sleep(std::time::Duration::from_secs(1));
         }
     }
 
     async fn run(&self) -> anyhow::Result<()> {
+        // Component
+        let component_pangolin_subxt = DarwiniaSubxtComponent::restore::<PangolinRopstenTask>()?;
+
+        // Darwinia client
+        let darwinia = component_pangolin_subxt.component().await?;
+
         let records = self.tracker.parallel_records()?;
         for block in records {
-            // todo: check redeem and remove it from kv db
+            let verified = match self.is_verified(&darwinia, block).await {
+                Ok(v) => v,
+                Err(e) => {
+                    // todo: save error times of block
+                    log::error!("Failed to get block verified state: [{}] {:?}", block, e);
+                    false
+                }
+            };
+            if !verified {
+                continue;
+            }
+            if let Err(e) = self.tracker.finish(block) {
+                log::error!("Failed to set finish to kv: [{}] {:?}", block, e);
+            }
         }
         Ok(())
+    }
+
+    async fn is_verified(&self, darwinia: &Darwinia, block: usize) -> anyhow::Result<bool> {
+        Ok(self
+            .darwinia_client
+            .verified(tx.block_hash, tx.index)
+            .await?
+            || self
+                .darwinia_client
+                .verified_issuing(tx.block_hash, tx.index)
+                .await?)
     }
 }

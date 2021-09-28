8a759fd6e65aa562fa732f38d65ab8c298bd66d5
fewensa <fewensa@protonmail.com>
2021-09-28 10:49:46 +0800

    Simplify tracker

--- supports/support-tracker/src/lib.rs
+++ supports/support-tracker/src/lib.rs
@@ -2,10 +2,15 @@ use itertools::Itertools;
 use microkv::namespace::NamespaceMicroKV;
 use std::fmt::{Debug, Formatter};
 
+// discuss: https://github.com/darwinia-network/bridger/issues/284
+/// Tracker
+/// Block scan status
 #[derive(Clone)]
 pub struct Tracker {
     microkv: NamespaceMicroKV,
+    /// Current scan value, the next value is current+1
     key_current: String,
+    /// Planned to execute, after to running the next value is planned+1
     key_planned: String,
 }
 
@@ -21,6 +26,7 @@ impl Debug for Tracker {
 }
 
 impl Tracker {
+    /// Create a new tracker, the key is prefix
     pub fn new(microkv: NamespaceMicroKV, key: impl AsRef<str>) -> Self {
         let key = key.as_ref();
         Self {
@@ -28,6 +34,66 @@ impl Tracker {
             key_current: format!("{}.current", key),
             key_planned: format!("{}.planned", key),
         }
+    }
+}
+
+impl Tracker {
+    pub fn is_running(&self) -> anyhow::Result<bool> {
+        self.read_bool(&self.key_running)
+    }
+
+    pub fn stop_running(&self) -> anyhow::Result<()> {
+        self.microkv
+            .put(&self.key_running, &String::from("false"))?;
+        Ok(())
+    }
+
+    pub fn start_running(&self) -> anyhow::Result<()> {
+        self.microkv.put(&self.key_running, &String::from("true"))?;
+        Ok(())
+    }
+
+    /// Read the next value
+    /// Generally the value is current+1, but if the planned have value, will use planned+1
+    pub async fn next(&self) -> anyhow::Result<usize> {
+        let is_running = self.is_running()?;
+        if !is_running {
+            loop {
+                let secs = 3;
+                log::warn!(
+                    "The track key [{}] isn't running (value is not `true`), wait {} seconds check again.",
+                    &self.key_running,
+                    secs
+                );
+                tokio::time::sleep(std::time::Duration::from_secs(secs)).await;
+                if self.is_running()? {
+                    break;
+                }
+            }
+        }
+
+        match self.read_u64(&self.key_planned)? {
+            Some(planned) => {
+                self.microkv.delete(&self.key_planned)?;
+                Ok(planned as usize)
+            }
+            None => {
+                let current: usize = self.microkv.get_as(&self.key_current)?.unwrap_or(0);
+                let next = current + 1;
+                Ok(next)
+            }
+        }
+    }
+
+    /// Update current
+    pub fn finish(&self, block: usize) -> anyhow::Result<()> {
+        self.microkv.put(&self.key_current, &block)?;
+        Ok(())
+    }
+
+    pub fn planned(&self, block: usize) -> anyhow::Result<()> {
+        self.microkv.put(&self.key_planned, &block)?;
+        Ok(())
     }
 }
 
@@ -71,77 +137,5 @@ impl Tracker {
             }
             None => Ok(None),
         }
-    }
-}
-
-impl Tracker {
-    pub fn is_running(&self) -> anyhow::Result<bool> {
-        self.read_bool(&self.key_running)
-    }
-
-    pub fn stop_running(&self) -> anyhow::Result<()> {
-        self.microkv
-            .put(&self.key_running, &String::from("false"))?;
-        Ok(())
-    }
-
-    pub fn start_running(&self) -> anyhow::Result<()> {
-        self.microkv.put(&self.key_running, &String::from("true"))?;
-        Ok(())
-    }
-
-    pub async fn next(&self) -> anyhow::Result<usize> {
-        let is_running = self.is_running()?;
-        if !is_running {
-            loop {
-                let secs = 3;
-                log::warn!(
-                    "The track key [{}] isn't running (value is not `true`), wait {} seconds check again.",
-                    &self.key_running,
-                    secs
-                );
-                tokio::time::sleep(std::time::Duration::from_secs(secs)).await;
-                if self.is_running()? {
-                    break;
-                }
-            }
-        }
-        let next: Option<String> = self.microkv.get_as(&self.key_next)?;
-        if next.is_none() {
-            let key = if self.is_fast_mode()? {
-                &self.key_curt
-            } else {
-                &self.key_finish
-            };
-            let curt: usize = self.microkv.get_as(key)?.unwrap_or(0);
-            let next = curt + 1;
-            self.microkv.put(&self.key_curt, &next)?;
-            return Ok(next);
-        }
-        let mut plan_blocks = parse_blocks_from_text(next.unwrap())?;
-        let next = *plan_blocks.first().unwrap_or(&1);
-        if !plan_blocks.is_empty() {
-            plan_blocks.remove(0);
-            let store_value: String = plan_blocks.iter().join(",");
-            if !plan_blocks.is_empty() {
-                self.microkv.put(&self.key_next, &store_value)?;
-            }
-        }
-        if plan_blocks.is_empty() {
-            self.microkv.delete(&self.key_next)?;
-        }
-        self.microkv.put(&self.key_curt, &next)?;
-        Ok(next)
-    }
-
-    pub fn finish(&self, block: usize) -> anyhow::Result<()> {
-        self.microkv.put(&self.key_finish, &block)?;
-        Ok(())
-    }
-
-    pub fn planned(&self, blocks: Vec<usize>) -> anyhow::Result<()> {
-        let value: String = blocks.iter().join(",");
-        self.microkv.put(&self.key_next, &value)?;
-        Ok(())
     }
 }

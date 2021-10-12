eecb25c32dd6cd887c9e5a600146c6813c4c4adf
fewensa <fewensa@protonmail.com>
2021-10-12 09:57:03 +0800

    Add backing handle

--- thegraph/src/backing.ts
+++ thegraph/src/backing.ts
@@ -8,62 +8,27 @@ import {
   RegistCompleted,
   VerifyProof
 } from "../generated/backing/backing"
-import { BackingEntity } from "../generated/schema"
+import { TransactionEntity} from "../generated/schema"
 
 export function handleBackingLock(event: BackingLock): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = BackingEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new BackingEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.sender = event.params.sender
-  entity.source = event.params.source
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.allAssets(...)
-  // - contract.assetLength(...)
-  // - contract.assets(...)
-  // - contract.crossChainSync(...)
-  // - contract.getIssuingEvent(...)
-  // - contract.history(...)
-  // - contract.owner(...)
-  // - contract.registerFee(...)
-  // - contract.relay(...)
-  // - contract.substrateEventStorageKey(...)
-  // - contract.transferFee(...)
-  // - contract.verifyProof(...)
+  let tx = new TransactionEntity(event.transaction.hash.toHex());
+  tx.origin = 'Backing';
+  tx.blockNumber = event.block.number;
+  tx.blockHash = event.block.hash;
+  tx.txHash = event.transaction.hash;
+  tx.txIndex = event.transaction.index;
+  tx.save()
 }
 
-export function handleNewTokenRegistered(event: NewTokenRegistered): void {}
+export function handleNewTokenRegistered(event: NewTokenRegistered): void {
+  let tx = new TransactionEntity(event.transaction.hash.toHex());
+  tx.origin = 'Backing';
+  tx.blockNumber = event.block.number;
+  tx.blockHash = event.block.hash;
+  tx.txHash = event.transaction.hash;
+  tx.txIndex = event.transaction.index;
+  tx.save()
+}
 
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
 

e4ec0bdcb6909813dd7bd6020a2a33d6891c40f0
fewensa <fewensa@protonmail.com>
2021-11-09 19:28:22 +0800

    fix test

--- components/client-pangolin-s2s/tests/common.rs
+++ components/client-pangolin-s2s/tests/common.rs
@@ -6,9 +6,9 @@ use component_pangolin_s2s::PangolinChain;
 async fn client() -> anyhow::Result<Client<PangolinChain>> {
     Ok(
         relay_substrate_client::Client::new(relay_substrate_client::ConnectionParams {
-            host: "127.0.0.1".to_string(),
-            port: 9955,
-            secure: false,
+            host: "pangolin-rpc.darwinia.network".to_string(),
+            port: 443,
+            secure: true,
         })
         .await,
     )

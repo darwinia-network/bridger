c533e66f56fd152eb4197374c353445a8b4bec82
fewensa <fewensa@protonmail.com>
2021-11-23 15:46:24 +0800

    fix tests

--- components/client-darwinia-s2s/tests/common.rs
+++ components/client-darwinia-s2s/tests/common.rs
@@ -3,7 +3,7 @@ use relay_substrate_client::Client;
 use component_darwinia_s2s::api::DarwiniaApi;
 use component_darwinia_s2s::DarwiniaChain;
 
-async fn client() -> anyhow::Result<Client<PangolinChain>> {
+async fn client() -> anyhow::Result<Client<DarwiniaChain>> {
     Ok(
         relay_substrate_client::Client::new(relay_substrate_client::ConnectionParams {
             host: "rpc.darwinia.network".to_string(),

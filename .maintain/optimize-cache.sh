284b88f383fd759aa8ed4b8473cb163cb81be9b6
fewensa <fewensa@protonmail.com>
2021-11-24 12:30:24 +0800

    format

--- .maintain/optimize-cache.sh
+++ .maintain/optimize-cache.sh
@@ -15,24 +15,24 @@ rm -rf target/release/bridger.pdb
 rm -rf target/release/bridger
 rm -rf target/release/incremental
 
-cargo clean -p darwinia-bridger --release
+cargo clean --release -p darwinia-bridger
 
-cargo clean -p crab-runtime --release
-cargo clean -p darwinia-runtime --release
-cargo clean -p pangolin-runtime --release
-cargo clean -p pangoro-runtime --release
+cargo clean --release -p crab-runtime
+cargo clean --release -p darwinia-runtime
+cargo clean --release -p pangolin-runtime
+cargo clean --release -p pangoro-runtime
 
-cargo clean -p task-pangolin-pangoro --release
-cargo clean -p task-darwinia-crab --release
-cargo clean -p component-pangolin-s2s --release
-cargo clean -p component-pangoro-s2s --release
-cargo clean -p component-darwinia-s2s --release
-cargo clean -p component-crab-s2s --release
+cargo clean --release -p task-pangolin-pangoro
+cargo clean --release -p task-darwinia-crab
+cargo clean --release -p component-pangolin-s2s
+cargo clean --release -p component-pangoro-s2s
+cargo clean --release -p component-darwinia-s2s
+cargo clean --release -p component-crab-s2s
 
-cargo clean -p messages-relay --release
-cargo clean -p relay-utils --release
-cargo clean -p finality-relay --release
-cargo clean -p substrate-relay-helper --release
-cargo clean -p relay-substrate-client --release
+cargo clean --release -p messages-relay
+cargo clean --release -p relay-utils
+cargo clean --release -p finality-relay
+cargo clean --release -p substrate-relay-helper
+cargo clean --release -p relay-substrate-client
 
 

3ef1f5a030b2fd55c6bbdb939f0b0e802769efb6
fewensa <fewensa@protonmail.com>
2021-11-24 11:23:51 +0800

    Modify the execution order

--- .maintain/optimize-cache.sh
+++ .maintain/optimize-cache.sh
@@ -8,16 +8,17 @@ WORK_PATH=${BIN_PATH}/../
 
 cd ${WORK_PATH}
 
-cargo clean -p crab-runtime --release
-cargo clean -p darwinia-runtime --release
-cargo clean -p pangolin-runtime --release
-cargo clean -p pangoro-runtime --release
-
-cargo clean -p darwinia-bridger --release
-
 
 rm -rf target/release/wbuild
 rm -rf target/release/build
 rm -rf target/release/bridger.pdb
 rm -rf target/release/bridger
 rm -rf target/release/incremental
+
+cargo clean -p darwinia-bridger --release
+
+cargo clean -p crab-runtime --release
+cargo clean -p darwinia-runtime --release
+cargo clean -p pangolin-runtime --release
+cargo clean -p pangoro-runtime --release
+

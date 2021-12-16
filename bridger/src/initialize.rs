b4d6ab169f2b7eabbe5f96333f431808bff39a2d
fewensa <fewensa@protonmail.com>
2021-12-16 16:58:54 +0800

    init

---
+++ bridger/src/initialize.rs
@@ -0,0 +1,30 @@
+use tracing::Level;
+
+pub fn init() -> color_eyre::Result<()> {
+    init_log()?;
+    Ok(())
+}
+
+fn init_log() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    if std::env::var("RUST_BACKTRACE").is_err() {
+        std::env::set_var("RUST_SPANTRACE", "1");
+        std::env::set_var("RUST_LIB_BACKTRACE", "full");
+    }
+    let log_filter = std::env::var("RUST_LOG").unwrap_or_else(|_| {
+        let filter = "trace,hyper=error";
+        std::env::set_var("RUST_LOG", filter);
+        filter.to_string()
+    });
+
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        .with_env_filter(log_filter)
+        // builds the subscriber.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+    Ok(())
+}
--- bridger/src/main.rs
+++ bridger/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+use std::process;
+
+mod initialize;
+
+fn main() -> color_eyre::Result<()> {
+    initialize::init()?;
     println!("Hello, world!");
+    Ok(())
 }

88ecd9f3ceb21608a1dbbffdaff3a0fe7d59d43b
fewensa <fewensa@protonmail.com>
2021-12-17 11:28:46 +0800

    Restruct command types and add reqwest

--- bridger/src/command/types/opt.rs
+++ bridger/src/command/types/opt.rs
@@ -3,6 +3,8 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(name = "bridger", about = "Darwinia bridger")]
 pub enum Opt {
+    /// Bridger registroy,
+    Registry,
     /// List all commands
     List,
 }

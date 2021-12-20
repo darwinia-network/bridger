7b388fb6981d1c5aa60b866bfd56ad4e23e817f0
fewensa <fewensa@protonmail.com>
2021-12-20 19:11:01 +0800

    test sub command

--- bin/src/main.rs
+++ bin/src/main.rs
@@ -10,6 +10,20 @@ mod initialize;
 
 fn main() -> color_eyre::Result<()> {
     initialize::init()?;
-    let opt = Opt::from_args();
-    cli::execute(opt)
+
+    let ret: Result<Opt, structopt::clap::Error> = Opt::from_args_safe();
+    match ret {
+        Ok(opt) => cli::execute(opt)?,
+        Err(e) => {
+            if structopt::clap::ErrorKind::UnknownArgument == e.kind {
+                let args_orig: Vec<String> = std::env::args().collect();
+                let sub_command = &args_orig[1..2][0];
+                let args_sub = &args_orig[2..];
+                println!("{} {:?}", sub_command, args_sub);
+            }
+            e.exit();
+        }
+    }
+
+    Ok(())
 }

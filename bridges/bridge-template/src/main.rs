125779dc26c0cd5d43cbe56fe478820991967141
fewensa <fewensa@protonmail.com>
2021-12-22 12:10:04 +0800

    Common initialize crate

--- bridges/bridge-template/src/main.rs
+++ bridges/bridge-template/src/main.rs
@@ -1,9 +1,14 @@
 #[tokio::main]
-async fn main() {
+async fn main() -> color_eyre::Result<()> {
+    support_initialize::init()?;
+
     let mut times = 0;
     loop {
         times += 1;
-        println!("bridge-template Count: {}", times);
+        if times > 5 {
+            return Err(color_eyre::Report::msg("Test error"));
+        }
+        tracing::debug!("bridge-template Count: {}", times);
         tokio::time::sleep(std::time::Duration::from_secs(2)).await;
     }
 }

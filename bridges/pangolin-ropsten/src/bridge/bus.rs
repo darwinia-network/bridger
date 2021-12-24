d0c3b9bae441d0f13b1ed7494d1fabed318ee766
fewensa <fewensa@protonmail.com>
2021-12-24 18:04:52 +0800

    Move config/bus to bridge mode

---
+++ bridges/pangolin-ropsten/src/bridge/bus.rs
@@ -0,0 +1,4 @@
+use lifeline::prelude::*;
+use lifeline::{Receiver, Sender};
+
+lifeline_bus!(pub struct PangolinRopstenBus);
--- bridges/pangolin-ropsten/src/lib.rs
+++ bridges/pangolin-ropsten/src/lib.rs
@@ -1,9 +1,6 @@
-pub mod config;
-pub mod task;
+mod bridge;
 
-mod bus;
 mod helpers;
-mod message;
 mod migrate;
 mod route;
 mod service;

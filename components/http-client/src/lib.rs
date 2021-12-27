807e3bfdb8edf2de6a57755f703371bd7a4a47e9
fewensa <fewensa@protonmail.com>
2021-12-27 15:45:40 +0800

    new component initialize for http client

--- components/http-client/src/lib.rs
+++ components/http-client/src/lib.rs
@@ -2,16 +2,19 @@ use serde::{Deserialize, Serialize};
 
 /// Http client config
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
-pub struct HttpClientVendor {
+pub struct HttpClientConfig {
     /// The timeout of request
     pub timeout: u64,
 }
 
-impl HttpClientVendor {
+/// Http client component
+pub struct HttpClientComponent;
+
+impl HttpClientComponent {
     /// Get request client
-    pub fn component(&self) -> color_eyre::Result<reqwest::Client> {
+    pub fn component(config: HttpClientConfig) -> color_eyre::Result<reqwest::Client> {
         Ok(reqwest::ClientBuilder::new()
-            .timeout(std::time::Duration::from_secs(self.timeout))
+            .timeout(std::time::Duration::from_secs(config.timeout))
             .build()?)
     }
 }

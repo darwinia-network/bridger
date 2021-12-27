8c95faa052333b5bdd5aca7b77dcb9a66f20d0cb
fewensa <fewensa@protonmail.com>
2021-12-27 18:21:06 +0800

    better export

--- components/subquery/src/lib.rs
+++ components/subquery/src/lib.rs
@@ -1,5 +1,11 @@
-pub mod component;
-pub mod config;
-pub mod error;
-pub mod subquery;
-pub mod types;
+pub use self::component::*;
+pub use self::config::*;
+pub use self::error::*;
+pub use self::subquery::*;
+pub use self::types::*;
+
+mod component;
+mod config;
+mod error;
+mod subquery;
+mod types;

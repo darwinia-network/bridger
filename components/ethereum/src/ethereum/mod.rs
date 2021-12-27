a3fdf76e22e601b6707c0e31e60c513cbfaa05f6
fewensa <fewensa@protonmail.com>
2021-12-27 12:24:10 +0800

    Code clean

--- components/ethereum/src/ethereum/mod.rs
+++ components/ethereum/src/ethereum/mod.rs
@@ -6,33 +6,6 @@ use crate::ethereum::client::{EthereumClient, EthereumConfig};
 pub mod client;
 pub mod types;
 
-// #[derive(Clone, Debug, Default)]
-// pub struct EthereumComponent {
-//     config: EthereumConfig,
-//     web3_component: Web3Component,
-// }
-
-// #[async_trait::async_trait]
-// impl BridgeComponent<EthereumConfig, EthereumClient> for EthereumComponent {
-//     fn restore_with_namespace<T: BridgeSand>(namespace: String) -> BridgeResult<Self>
-//     where
-//         Self: Sized,
-//     {
-//         let config: EthereumConfig = Config::restore_with_namespace_unwrap(T::NAME, &namespace)?;
-//         let web3_component = Web3Component::restore_with_namespace::<T>(namespace)?;
-//         Ok(Self::new(config, web3_component))
-//     }
-//
-//     async fn component(&self) -> anyhow::Result<EthereumClient> {
-//         let web3 = self.web3_component.component().await?;
-//         Ok(EthereumClient::new(self.config.clone(), web3))
-//     }
-//
-//     fn config(&self) -> &EthereumConfig {
-//         &self.config
-//     }
-// }
-
 /// Ethereum provider
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct EthereumProvider {

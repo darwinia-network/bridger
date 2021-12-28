1fcc9028e73b45fea905b332fd85aa8dbf5f707f
fewensa <fewensa@protonmail.com>
2021-12-28 18:25:50 +0800

    Fix bridge template

--- bridges/template/src/bridge/config.rs
+++ bridges/template/src/bridge/config.rs
@@ -1,10 +1,10 @@
 use serde::{Deserialize, Serialize};
 
-use component_http_client::HttpClientVendor;
+use component_http_client::HttpClientConfig;
 
 /// Bridge template config
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct TemplateTaskConfig {
     /// Http client config
-    pub http_client: HttpClientVendor,
+    pub http_client: HttpClientConfig,
 }

52f63522fa95bc2676a22b23f500786778752ea2
fewensa <fewensa@protonmail.com>
2021-12-31 12:15:36 +0800

    remove useless arg

--- bridger.sh
+++ bridger.sh
@@ -7,7 +7,7 @@ BIN_PATH=$(dirname $(readlink -f $0))
 
 cargo build \
   --manifest-path $BIN_PATH/frame/Cargo.toml \
-  --package darwinia-bridger \
-  --bin bridger
+  --package darwinia-bridger
+
 
 $BIN_PATH/frame/target/debug/bridger $@

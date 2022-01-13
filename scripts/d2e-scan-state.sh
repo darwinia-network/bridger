3a0fd72b432b3a050a89f9399925f6940b85f635
fewensa <fewensa@protonmail.com>
2022-01-13 15:20:43 +0800

    d2e scan state

---
+++ scripts/d2e-scan-state.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+set -xe
+
+BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
+WORK_PATH=${BIN_PATH}/../
+BRIDGER=${WORK_PATH}/bridger.sh
+
+NAMESPACE=$1
+
+${BRIDGER} kv -n ${NAMESPACE} get -o table --include-key \
+  scan.pangolin.running \
+  scan.ropsten.check.running \
+  scan.ropsten.check.current \
+  scan.ropsten.check.planned \
+  scan.ropsten.redeem.running \
+  scan.ropsten.redeem.current \
+  scan.ropsten.redeem.planned \
+  scan.ropsten.affirm.running \
+  scan.ropsten.affirm.current \
+  scan.ropsten.affirm.planned
+
+

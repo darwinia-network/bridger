0ebf94b843d8cbdb870eb5ea23af0946611b9f9b
fewensa <fewensa@protonmail.com>
2022-01-15 22:06:58 +0800

    clippy

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -7,32 +7,36 @@ WORK_PATH=${BIN_PATH}/../
 
 PACKAGE=$1
 
+_clippy() {
+  MANIFEST=$1
+  LOCKED=$2
+  cargo clippy \
+    --manifest-path ${MANIFEST} ${LOCKED} \
+    --all -- -D warnings
+}
 
 if [ "${PACKAGE}" == "bridger" ]; then
-  cargo clippy --manifest-path ${WORK_PATH}/frame/Cargo.toml \
-    --all -- -D warnings
+  _clippy ${WORK_PATH}/frame/Cargo.toml
 fi
 
 if [ "${PACKAGE}" == "pangolin-ropsten" ]; then
-  cargo clippy --manifest-path ${WORK_PATH}/bridges/pangolin-ropsten/Cargo.toml \
-    --all -- -D warnings
+  _clippy ${WORK_PATH}/bridges/pangolin-ropsten/Cargo.toml \
+    --locked
 fi
 
 if [ "${PACKAGE}" == "darwinia-ethereum" ]; then
-  cargo clippy --manifest-path ${WORK_PATH}/bridges/darwinia-ethereum/Cargo.toml \
-    --all -- -D warnings
+  _clippy ${WORK_PATH}/bridges/darwinia-ethereum/Cargo.toml \
+    --locked
 fi
 
 if [ "${PACKAGE}" == "pangolin-pangoro" ]; then
-  cargo clippy --manifest-path ${WORK_PATH}/bridges/pangolin-pangoro/Cargo.toml \
-    --all -- -D warnings
+  _clippy --manifest-path ${WORK_PATH}/bridges/pangolin-pangoro/Cargo.toml \
+    --locked
 fi
 
 if [ "${PACKAGE}" == "darwinia-crab" ]; then
-  cargo clippy --manifest-path ${WORK_PATH}/bridges/darwinia-crab/Cargo.toml \
-    --all -- -D warnings
+  _clippy ${WORK_PATH}/bridges/darwinia-crab/Cargo.toml \
+    --locked
 fi
-
-
 
 

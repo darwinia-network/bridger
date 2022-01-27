8362776a2f7c1287e9960c2b9b8a88f17a647114
fewensa <fewensa@protonmail.com>
2022-01-27 10:03:21 +0800

    connect web3 and pangolin subxt client types

--- frame/assistants/pangolin-subxt/src/lib.rs
+++ frame/assistants/pangolin-subxt/src/lib.rs
@@ -1,5 +1,9 @@
-#![allow(dead_code)]
-
+pub use self::error::*;
 pub use self::runtime::*;
+pub use self::x_web3::*;
 
+#[rustfmt::skip]
+#[allow(clippy::all)]
 mod runtime;
+mod error;
+mod x_web3;

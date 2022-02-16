83c50fbce8a1d7831d43a4df2a24e4864dfd2852
furoxr <furuoxuan@wcoaltech.com>
2022-02-16 16:33:10 +0800

    Add pangoro subxt client tests

--- bridges/pangoro-chapel/client-pangoro/tests/common.rs
+++ bridges/pangoro-chapel/client-pangoro/tests/common.rs
@@ -1,13 +1,20 @@
 use client_pangoro::client::PangoroClient;
 use client_pangoro::component::PangoroClientComponent;
 use client_pangoro::config::ClientConfig;
+use web3::transports::Http;
+use web3::Web3;
 
 pub async fn client() -> color_eyre::Result<PangoroClient> {
     let config = ClientConfig {
-        endpoint: "ws://127.0.0.1:9944".to_string(),
+        endpoint: "wss://pangoro-rpc.darwinia.network:443".to_string(),
         relayer_private_key: "//Alice".to_string(),
         relayer_real_account: None,
         ecdsa_authority_private_key: None,
     };
     PangoroClientComponent::component(config).await
+}
+
+pub fn web3_client() -> color_eyre::Result<Web3<Http>> {
+    let transport = web3::transports::Http::new("https://data-seed-prebsc-2-s3.binance.org:8545/")?;
+    Ok(web3::Web3::new(transport))
 }

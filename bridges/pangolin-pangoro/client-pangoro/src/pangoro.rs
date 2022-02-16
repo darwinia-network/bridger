7a7ca028b37b680c5cb2d834fdf83631a97897e2
fewensa <fewensa@protonmail.com>
2022-02-16 10:59:56 +0800

    Fix pangoro client

--- bridges/pangolin-pangoro/client-pangoro/src/pangoro.rs
+++ bridges/pangolin-pangoro/client-pangoro/src/pangoro.rs
@@ -128,11 +128,11 @@ impl TransactionSignScheme for PangoroChain {
     }
 
     fn is_signed(tx: &Self::SignedTransaction) -> bool {
-        tx.signature.is_some()
+        tx.0.signature.is_some()
     }
 
     fn is_signed_by(signer: &Self::AccountKeyPair, tx: &Self::SignedTransaction) -> bool {
-        tx.signature
+        tx.0.signature
             .as_ref()
             .map(|(address, _, _)| {
                 let account_id: drml_common_primitives::AccountId =
@@ -143,9 +143,9 @@ impl TransactionSignScheme for PangoroChain {
     }
 
     fn parse_transaction(tx: Self::SignedTransaction) -> Option<UnsignedTransaction<Self::Chain>> {
-        let extra = &tx.signature.as_ref()?.2;
+        let extra = &tx.0.signature.as_ref()?.2;
         Some(UnsignedTransaction {
-            call: tx.function,
+            call: tx.0.function,
             nonce: Compact::<IndexOf<Self::Chain>>::decode(&mut &extra.4.encode()[..])
                 .ok()?
                 .into(),

e9f68ccf41e329cae9d843e36257f8d4e9be5df0
fewensa <fewensa@protonmail.com>
2022-05-10 11:18:38 +0800

    Remove shadow component

--- frame/components/shadow/src/error.rs
+++
@@ -1,8 +0,0 @@
-use thiserror::Error as ThisError;
-
-/// Ethereum component error
-#[derive(ThisError, Debug)]
-pub enum ShadowComponentError {
-    #[error("Internal server error: {0}")]
-    InternalServer(String),
-}

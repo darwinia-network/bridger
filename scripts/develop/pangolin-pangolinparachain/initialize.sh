750b6fa3aca927e9f4d7788ca438780f82b459fc
fewensa <fewensa@protonmail.com>
2022-05-13 17:08:27 +0800

    Try fix build

--- scripts/develop/pangolin-pangolinparachain/initialize.sh
+++ scripts/develop/pangolin-pangolinparachain/initialize.sh
@@ -12,11 +12,15 @@ CONTAINER_SUBQL_BUILD=subql-build
 EXEC_PG="docker exec -it ${CONTAINER_POSTGRES}"
 EXEC_SUBQL_BUILD="docker exec -it ${CONTAINER_SUBQL_BUILD}"
 
+DATABASES='subql_s2s_pangolin subql_s2s_pangolin_parachain subql_s2s_rococo subql_parachain_rococo'
 
-${EXEC_PG} psql -U postgres -c 'create database subql_s2s_pangolin'
-${EXEC_PG} psql -U postgres -c 'create database subql_s2s_pangolin_parachain'
-${EXEC_PG} psql -U postgres -c 'create database subql_s2s_rococo'
-${EXEC_PG} psql -U postgres -c 'create database subql_parachain_rococo'
+
+for DB in ${DATABASES}; do
+  ${EXEC_PG} \
+    psql -U postgres \
+      -tc "select 1 from pg_database where datname = '${DB}'" | grep -q 1 || \
+    psql -U postgres -c "create database ${DB}"
+done
 
 docker stop ${CONTAINER_SUBQL_BUILD} || true
 docker rm ${CONTAINER_SUBQL_BUILD} || true
@@ -27,16 +31,11 @@ docker run -dit \
   -v ${WORKSPACE}/subql:/data \
   node:14
 
-docker run -dit \
-  --rm \
-  --name ${CONTAINER_SUBQL_BUILD} \
-  -v ${PWD}/subql:/data \
-  node:14
+SUBQL_PROJECTS='s2s/pangolin s2s/pangolin-parachain s2s/rococo parachain/rococo'
 
-${EXEC_SUBQL_BUILD} sh -c "cd /data/s2s/pangolin && npm i && npm run codegen && npm run build"
-${EXEC_SUBQL_BUILD} sh -c "cd /data/s2s/pangolin-parachain && npm i && npm run codegen && npm run build"
-${EXEC_SUBQL_BUILD} sh -c "cd /data/s2s/rococo && npm i && npm run codegen && npm run build"
-${EXEC_SUBQL_BUILD} sh -c "cd /data/parachain/rococo && npm i && npm run codegen && npm run build"
+for PROJ in ${SUBQL_PROJECTS}; do
+  ${EXEC_SUBQL_BUILD} sh -c "cd /data/${PROJ} && npm i && npm run codegen && npm run build"
+done
 
 docker stop ${CONTAINER_SUBQL_BUILD}
 docker rm ${CONTAINER_SUBQL_BUILD}

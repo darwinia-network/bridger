ba860e2d0ac1591986aa92008b442784623bd068
nada <37610161+furoxr@users.noreply.github.com>
2022-05-13 13:44:12 +0800

    Update script

--- scripts/develop/pangolin-pangolinparachain/entrypoint/run-rococo.sh
+++ scripts/develop/pangolin-pangolinparachain/entrypoint/run-rococo.sh
@@ -5,7 +5,7 @@ BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
 
 ACCOUNT=$1
 DATA_PATH=/data/node-data/rococo-${ACCOUNT}
-
+mkdir -p ${DATA_PATH}
 polkadot build-spec --chain rococo-local --disable-default-bootnode --raw > ${DATA_PATH}/rococo-local-cfde.json
 
 polkadot \

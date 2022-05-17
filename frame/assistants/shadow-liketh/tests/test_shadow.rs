ace160af9ec827765ffb4e04f49a211c7e75f597
fewensa <fewensa@protonmail.com>
2022-05-17 15:44:47 +0800

    Test darwinia-ethereum

--- frame/assistants/shadow-liketh/tests/test_shadow.rs
+++ frame/assistants/shadow-liketh/tests/test_shadow.rs
@@ -14,11 +14,11 @@ async fn test_mmr_root_with_proof_about_ropsten() {
         .unwrap();
     let parent_mmr_root_hex = array_bytes::bytes2hex("0x", parent_mmr_root);
     println!(
-        "PARENT MMR ROOT FOR BLOCK {}: {}",
+        "[ROPSTEN] PARENT MMR ROOT FOR BLOCK {}: {}",
         block, parent_mmr_root_hex,
     );
     println!(
-        "MMR PROOF FOR MEMBER_LEAF_INDEX: {} AND LEAST_LEAF_INDEX: {}",
+        "[ROPSTEN] MMR PROOF FOR MEMBER_LEAF_INDEX: {} AND LEAST_LEAF_INDEX: {}",
         member_leaf_index, last_leaf_index,
     );
     for item in mmr_proof {
@@ -32,6 +32,50 @@ async fn test_receipt_about_ropsten() {
     let tx_hash = "0x5c1443fb7d0e478a53e7d96ec05a37ca07be167edf8763fa655e508755e2e1e9";
     let last = 12271421;
     let shadow = common::shadow(Network::Ropsten);
+    let receipt = shadow.receipt(tx_hash, last).await.unwrap();
+    let mmr_proof = receipt.mmr_proof;
+    let receipt_proof_json = serde_json::to_string_pretty(&receipt.receipt).unwrap();
+    println!("RECEIPT PROOF JSON: {}", receipt_proof_json);
+    println!(
+        "MMR PROOF FOR MEMBER_LEAF_INDEX: {} AND LEAST_LEAF_INDEX: {}",
+        mmr_proof.member_leaf_index, mmr_proof.last_leaf_index,
+    );
+    for item in mmr_proof.proof {
+        let hex = array_bytes::bytes2hex("", item);
+        println!("{}", hex);
+    }
+}
+
+#[tokio::test]
+async fn test_mmr_root_with_proof_about_ethereum() {
+    let block = 14779883;
+    let shadow = common::shadow(Network::Ethereum);
+    let parent_mmr_root = shadow.mmr_root(block - 1).await.unwrap();
+    let (member_leaf_index, last_leaf_index) = (block - 1, block);
+    let mmr_proof = shadow
+        .mmr_proof(member_leaf_index, last_leaf_index)
+        .await
+        .unwrap();
+    let parent_mmr_root_hex = array_bytes::bytes2hex("0x", parent_mmr_root);
+    println!(
+        "[ETHEREUM] PARENT MMR ROOT FOR BLOCK {}: {}",
+        block, parent_mmr_root_hex,
+    );
+    println!(
+        "[ETHEREUM] MMR PROOF FOR MEMBER_LEAF_INDEX: {} AND LEAST_LEAF_INDEX: {}",
+        member_leaf_index, last_leaf_index,
+    );
+    for item in mmr_proof {
+        let hex = array_bytes::bytes2hex("", item);
+        println!("{}", hex);
+    }
+}
+
+#[tokio::test]
+async fn test_receipt_about_ethereum() {
+    let tx_hash = "0x82125e0829110f9605420671bdb766e4195e37ceef2d250b37961211f1f61ce1";
+    let last = 14779883;
+    let shadow = common::shadow(Network::Ethereum);
     let receipt = shadow.receipt(tx_hash, last).await.unwrap();
     let mmr_proof = receipt.mmr_proof;
     let receipt_proof_json = serde_json::to_string_pretty(&receipt.receipt).unwrap();

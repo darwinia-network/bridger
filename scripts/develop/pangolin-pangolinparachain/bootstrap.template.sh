0af792a28cbef1f17f47b4ddc67d76b5c966c676
fewensa <37804932+fewensa@users.noreply.github.com>
2022-05-20 13:38:35 +0800

    Contributor Guide (#460)
    * Init s2s docker-compose
    
    * Revert s2s docker-compose
    
    * Try generate docker compose
    
    * Generate docker compose yml
    
    * Bootstrap script
    
    * Read env first
    
    * Format
    
    * Try rename usage
    
    * Rename usage
    
    * Update script
    
    * Add docs
    
    * Update scripts
    
    * Use bash
    
    * chmod +x
    
    * Call generate.sh directly
    
    * chmod +x
    
    * Add bridger to compose
    
    * Try fix build
    
    * Fix wrong script
    
    * Fix shell
    
    * Fix shell
    
    * Fix build
    
    * Add parachain rococo subql to bootstrap and init data
    
    * chmod +x
    
    * Fix docker-compose
    
    * Try fix build
    
    * Fix build
    
    * docs
    
    * Add subql project template file and update docs
    
    * Fix wrong pg init database command
    
    * Fix CI
    
    * Revert CI
    
    * Update force argument
    
    * Add bridger to docker compose and init bridger home
    
    * Fix scripts
    
    * Fix scripts
    
    Co-authored-by: nada <37610161+furoxr@users.noreply.github.com>

---
+++ scripts/develop/pangolin-pangolinparachain/bootstrap.template.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Please copy this file to bootstrap.local.sh and modify your variables
+
+set -xe
+
+BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
+
+# Cargo home
+export CARGO_HOME=${CARGO_HOME}
+export RUSTUP_HOME=${RUSTUP_HOME}
+
+# Data path, include pangolin/pangolin-parachain/rococo/bridger/subql data
+export DATA_DIR=/tmp/bridger/pangolin-pangolinparachain
+# Pangolin parachain source code path
+export PANGOLIN_PARACHAIN_SOURCE=/path/to/darwinia-parachain
+# Pangolin source code path
+export PANGOLIN_SOURCE=/path/to/darwinia-common
+
+export SUBQL_S2S_PANGOLIN_PORT=13000
+export SUBQL_S2S_PANGOLIN_PARACHAIN_PORT=13001
+export SUBQL_S2S_ROCOCO_PORT=13002
+export SUBQL_PARACHAIN_ROCOCO_PORT=13003
+
+export ROCOCO_ALICE_PORT_WS=19901
+export ROCOCO_BOB_PORT_WS=19902
+
+export PANGOLIN_PARACHAIN_PORT_WS=19701
+export PANGOLIN_PORT_WS=19801
+
+export BRIDGER_HOME=${BRIDGER_HOME:-${DATA_DIR}/bridger}
+
+FORCE=$1
+
+DOCKER_COMPOSE="docker-compose -f ${BIN_PATH}/docker-compose.yml"
+
+${BIN_PATH}/generate.sh
+
+if [ "${FORCE}" == "force" ]; then
+  for FOLDER in $(ls ${DATA_DIR}); do
+    if [ "$FOLDER" == "bridger" ]; then
+      continue
+    fi
+    rm -rf ${FOLDER}
+  done
+
+  ${DOCKER_COMPOSE} up -d postgres
+
+  ${BIN_PATH}/initialize.sh
+fi
+
+${DOCKER_COMPOSE} up
---
+++ scripts/develop/pangolin-pangolinparachain/entrypoint/run-pangolin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+
+
+BIN_PATH=$(cd "$(dirname "$0")"; pwd -P)
+SOURCE_PATH=/data/source
+DATA_PATH=/data/node-data/pangolin
+mkdir -p ${DATA_PATH}
+DRML=${SOURCE_PATH}/target/release/drml
+
+if [ ! -f ${DRML} ]; then
+  apt update -y
+  apt install -y libclang-dev
+fi
+
+cd ${SOURCE_PATH}
+cargo build --release
+
+${DRML} \
+   --chain pangolin-dev \
+   --base-path ${DATA_PATH} \
+   --rpc-cors all \
+   --unsafe-ws-external \
+   --alice
+

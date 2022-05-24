ca0eba1f01d35735d16e24fb7832ecc3d1d7572f
fewensa <fewensa@protonmail.com>
2022-05-24 18:16:21 +0800

    Fix subscribe types

--- frame/assistants/client-pangoro/src/fastapi/s2s/generic.rs
+++ frame/assistants/client-pangoro/src/fastapi/s2s/generic.rs
@@ -1,10 +1,11 @@
 use subxt::rpc::{Subscription, SubscriptionClientT};
+use subxt::sp_core;
 
 use crate::client::PangoroClient;
 use crate::error::ClientResult;
 
 impl PangoroClient {
-    pub async fn subscribe_justification(&self) -> ClientResult<Subscription<Vec<u8>>> {
+    pub async fn subscribe_justification(&self) -> ClientResult<Subscription<sp_core::Bytes>> {
         Ok(self
             .subxt()
             .rpc()

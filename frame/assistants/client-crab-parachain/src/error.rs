25514f7eff0e1965ed77027727988f055ed60996
nada <37610161+furoxr@users.noreply.github.com>
2022-06-02 10:43:01 +0800

    Add client-crab-parachain

---
+++ frame/assistants/client-crab-parachain/src/error.rs
@@ -0,0 +1,41 @@
+#![allow(missing_docs)]
+
+use support_toolkit::error::TkError;
+use thiserror::Error as ThisError;
+
+pub type ClientResult<T> = Result<T, ClientError>;
+
+/// Error enum.
+#[derive(ThisError, Debug)]
+pub enum ClientError {
+    #[error(transparent)]
+    SubxtBasicError(subxt::BasicError),
+
+    #[error("Please reconnect to rpc server")]
+    ClientRestartNeed,
+
+    #[error("Wrong seed: {0}")]
+    Seed(String),
+
+    #[error(transparent)]
+    Tk(#[from] TkError),
+
+    #[error("Other error: {0}")]
+    Other(String),
+}
+
+impl ClientError {
+    /// Is restart need error
+    pub fn is_restart_need(&self) -> bool {
+        matches!(self, Self::ClientRestartNeed)
+    }
+}
+
+impl From<subxt::BasicError> for ClientError {
+    fn from(error: subxt::BasicError) -> Self {
+        if let subxt::BasicError::Rpc(_) = &error {
+            return Self::ClientRestartNeed;
+        }
+        Self::SubxtBasicError(error)
+    }
+}

dc793d9ec35f35beb9f4177dc6e2a58c0b196700
fewensa <fewensa@protonmail.com>
2022-06-16 15:48:11 +0800

    add abstract client s2s for pangoro client

--- frame/assistants/client-pangoro/src/fastapi/s2s/generic.rs
+++ frame/assistants/client-pangoro/src/fastapi/s2s/generic.rs
@@ -4,6 +4,8 @@ use sp_runtime::{ConsensusEngineId, DigestItem};
 use subxt::rpc::{ClientT, Subscription, SubscriptionClientT};
 use subxt::{sp_core, sp_runtime};
 
+use abstract_client_s2s::client::S2SClient;
+
 use crate::client::PangoroClient;
 use crate::error::{ClientError, ClientResult};
 use crate::types::runtime_types::bp_header_chain::InitializationData;
@@ -17,7 +19,9 @@ type BundleHeader = crate::types::runtime_types::sp_runtime::generic::header::He
 type SpHeader = sp_runtime::generic::Header<u32, sp_runtime::traits::BlakeTwo256>;
 
 impl PangoroClient {
-    pub async fn subscribe_grandpa_justifications(&self) -> ClientResult<Subscription<sp_core::Bytes>> {
+    pub async fn subscribe_grandpa_justifications(
+        &self,
+    ) -> ClientResult<Subscription<sp_core::Bytes>> {
         Ok(self
             .subxt()
             .rpc()
@@ -29,10 +33,71 @@ impl PangoroClient {
             )
             .await?)
     }
+}
 
-    pub async fn prepare_initialization_data(
+impl PangoroClient {
+    async fn grandpa_authorities(&self, at: sp_core::H256) -> ClientResult<AuthorityList> {
+        let params = subxt::rpc::rpc_params![
+            "GrandpaApi_grandpa_authorities",
+            sp_core::Bytes(Vec::new()),
+            at
+        ];
+        let hex: String = self
+            .subxt()
+            .rpc()
+            .client
+            .request("state_call", params)
+            .await?;
+        let raw_authorities_set = array_bytes::hex2bytes(hex)?;
+        let authorities = codec::Decode::decode(&mut &raw_authorities_set[..]).map_err(|err| {
+            ClientError::Custom(format!(
+                "[DecodeAuthorities] Can not decode authorities: {:?}",
+                err
+            ))
+        })?;
+        Ok(authorities)
+    }
+
+    /// Find header digest that schedules next GRANDPA authorities set.
+    fn find_grandpa_authorities_scheduled_change(
         &self,
-    ) -> ClientResult<InitializationData<BundleHeader>> {
+        header: &SpHeader,
+    ) -> Option<ScheduledChange<u32>> {
+        let filter_log = |log: ConsensusLog<u32>| match log {
+            ConsensusLog::ScheduledChange(change) => Some(change),
+            _ => None,
+        };
+
+        // find the first consensus digest with the right ID which converts to
+        // the right kind of consensus log.
+        header
+            .digest
+            .logs
+            .iter()
+            .filter_map(|item| match item {
+                DigestItem::Consensus(engine, logs) => {
+                    if engine == &GRANDPA_ENGINE_ID {
+                        Some(&logs[..])
+                    } else {
+                        None
+                    }
+                }
+                _ => None,
+            })
+            .find_map(|mut l| {
+                let log = codec::Decode::decode(&mut l).ok();
+                log.and_then(filter_log)
+            })
+    }
+}
+
+#[async_trait::async_trait]
+impl S2SClient for PangoroClient {
+    type Error = ClientError;
+    type Header = BundleHeader;
+    type InitializationData = InitializationData<BundleHeader>;
+
+    async fn prepare_initialization_data(&self) -> ClientResult<Self::InitializationData> {
         let mut subscription = self.subscribe_grandpa_justifications().await?;
         let justification = subscription
             .next()
@@ -147,61 +212,5 @@ impl PangoroClient {
         Ok(codec::Decode::decode(&mut &bytes[..]).map_err(|e| {
             ClientError::Custom(format!("Failed to decode initialization data: {:?}", e))
         })?)
-    }
-}
-
-impl PangoroClient {
-    async fn grandpa_authorities(&self, at: sp_core::H256) -> ClientResult<AuthorityList> {
-        let params = subxt::rpc::rpc_params![
-            "GrandpaApi_grandpa_authorities",
-            sp_core::Bytes(Vec::new()),
-            at
-        ];
-        let hex: String = self
-            .subxt()
-            .rpc()
-            .client
-            .request("state_call", params)
-            .await?;
-        let raw_authorities_set = array_bytes::hex2bytes(hex)?;
-        let authorities = codec::Decode::decode(&mut &raw_authorities_set[..]).map_err(|err| {
-            ClientError::Custom(format!(
-                "[DecodeAuthorities] Can not decode authorities: {:?}",
-                err
-            ))
-        })?;
-        Ok(authorities)
-    }
-
-    /// Find header digest that schedules next GRANDPA authorities set.
-    fn find_grandpa_authorities_scheduled_change(
-        &self,
-        header: &SpHeader,
-    ) -> Option<ScheduledChange<u32>> {
-        let filter_log = |log: ConsensusLog<u32>| match log {
-            ConsensusLog::ScheduledChange(change) => Some(change),
-            _ => None,
-        };
-
-        // find the first consensus digest with the right ID which converts to
-        // the right kind of consensus log.
-        header
-            .digest
-            .logs
-            .iter()
-            .filter_map(|item| match item {
-                DigestItem::Consensus(engine, logs) => {
-                    if engine == &GRANDPA_ENGINE_ID {
-                        Some(&logs[..])
-                    } else {
-                        None
-                    }
-                }
-                _ => None,
-            })
-            .find_map(|mut l| {
-                let log = codec::Decode::decode(&mut l).ok();
-                log.and_then(filter_log)
-            })
     }
 }

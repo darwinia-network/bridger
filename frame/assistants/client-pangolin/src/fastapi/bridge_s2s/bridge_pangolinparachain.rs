09cebe02b1c6279f4d45ab4ce1e0393d05adbfb8
nada <37610161+furoxr@users.noreply.github.com>
2022-06-24 18:22:02 +0800

    Add bridge_pangolinparachain to pangolin

---
+++ frame/assistants/client-pangolin/src/fastapi/bridge_s2s/bridge_pangolinparachain.rs
@@ -0,0 +1,252 @@
+use std::ops::RangeInclusive;
+
+use abstract_bridge_s2s::error::S2SClientResult;
+use abstract_bridge_s2s::{
+    client::S2SParaBridgeClientSolochain,
+    config::Config,
+    types::{bp_header_chain, bp_messages, bridge_runtime_common},
+};
+use sp_runtime::generic::{Block, SignedBlock};
+use sp_runtime::AccountId32;
+use subxt::sp_core::storage::StorageKey;
+use subxt::storage::StorageKeyPrefix;
+use subxt::StorageEntry;
+use support_toolkit::convert::SmartCodecMapper;
+
+use crate::client::PangolinClient;
+use crate::error::ClientError;
+use crate::subxt_runtime::api::bridge_pangolin_parachain_messages::storage::{
+    InboundLanes, OutboundLanes, OutboundMessages,
+};
+
+type BundleMessageKey = crate::types::runtime_types::bp_messages::MessageKey;
+
+/// Message payload for This -> Bridged chain messages.
+type FromThisChainMessagePayload = crate::types::runtime_types::bp_message_dispatch::MessagePayload<
+    sp_core::crypto::AccountId32,
+    crate::types::runtime_types::sp_runtime::MultiSigner,
+    crate::types::runtime_types::sp_runtime::MultiSignature,
+    Vec<u8>,
+>;
+
+#[async_trait::async_trait]
+impl S2SParaBridgeClientSolochain for PangolinClient {
+    fn gen_outbound_messages_storage_key(&self, lane: [u8; 4], message_nonce: u64) -> StorageKey {
+        let prefix = StorageKeyPrefix::new::<OutboundMessages>();
+        OutboundMessages(BundleMessageKey {
+            lane_id: lane,
+            nonce: message_nonce,
+        })
+        .key()
+        .final_key(prefix)
+    }
+
+    fn gen_outbound_lanes_storage_key(&self, lane: [u8; 4]) -> StorageKey {
+        OutboundLanes(lane)
+            .key()
+            .final_key(StorageKeyPrefix::new::<OutboundLanes>())
+    }
+
+    fn gen_inbound_lanes_storage_key(&self, lane: [u8; 4]) -> StorageKey {
+        InboundLanes(lane)
+            .key()
+            .final_key(StorageKeyPrefix::new::<InboundLanes>())
+    }
+
+    async fn calculate_dispatch_weight(
+        &self,
+        lane: [u8; 4],
+        nonces: RangeInclusive<u64>,
+    ) -> S2SClientResult<u64> {
+        let mut total_weight = 0u64;
+        for message_nonce in nonces {
+            let message_data = self
+                .outbound_messages(
+                    bp_messages::MessageKey {
+                        lane_id: lane,
+                        nonce: message_nonce,
+                    },
+                    None,
+                )
+                .await?
+                .ok_or_else(|| {
+                    ClientError::Custom(format!(
+                        "Can not read message data by nonce {} in pangolin",
+                        message_nonce
+                    ))
+                })?;
+            let decoded_payload: FromThisChainMessagePayload =
+                codec::Decode::decode(&mut &message_data.payload[..])?;
+            total_weight += decoded_payload.weight;
+        }
+        Ok(total_weight)
+    }
+
+    async fn header(
+        &self,
+        hash: Option<<Self::Config as Config>::Hash>,
+    ) -> S2SClientResult<Option<<Self::Config as Config>::Header>> {
+        match self.subxt().rpc().header(hash).await? {
+            Some(v) => Ok(Some(SmartCodecMapper::map_to(&v)?)),
+            None => Ok(None),
+        }
+    }
+
+    async fn block(
+        &self,
+        hash: Option<<Self::Config as Config>::Hash>,
+    ) -> S2SClientResult<
+        Option<SignedBlock<Block<<Self::Config as Config>::Header, Self::Extrinsic>>>,
+    > {
+        Ok(self.subxt().rpc().block(hash).await?)
+    }
+
+    async fn best_target_finalized(
+        &self,
+        at_block: Option<<Self::Config as Config>::Hash>,
+    ) -> S2SClientResult<<Self::Config as Config>::Hash> {
+        Err("not needed")
+    }
+
+    async fn submit_finality_proof(
+        &self,
+        finality_target: <Self::Config as Config>::Header,
+        justification: bp_header_chain::justification::GrandpaJustification<
+            <Self::Config as Config>::Header,
+        >,
+    ) -> S2SClientResult<<Self::Config as Config>::Hash> {
+        Err("not needed")
+    }
+
+    async fn outbound_lanes(
+        &self,
+        lane: [u8; 4],
+        hash: Option<<Self::Config as Config>::Hash>,
+    ) -> S2SClientResult<bp_messages::OutboundLaneData> {
+        let outbound_lane_data = self
+            .runtime()
+            .storage()
+            .bridge_pangolin_parachain_messages()
+            .outbound_lanes(lane, hash)
+            .await?;
+        let expected = SmartCodecMapper::map_to(&outbound_lane_data)?;
+        Ok(expected)
+    }
+
+    async fn inbound_lanes(
+        &self,
+        lane: [u8; 4],
+        hash: Option<<Self::Config as Config>::Hash>,
+    ) -> S2SClientResult<bp_messages::InboundLaneData<sp_core::crypto::AccountId32>> {
+        let inbound_lane_data = self
+            .runtime()
+            .storage()
+            .bridge_pangolin_parachain_messages()
+            .inbound_lanes(lane, hash)
+            .await?;
+        let expected = SmartCodecMapper::map_to(&inbound_lane_data)?;
+        Ok(expected)
+    }
+
+    async fn outbound_messages(
+        &self,
+        message_key: bp_messages::MessageKey,
+        hash: Option<<Self::Config as Config>::Hash>,
+    ) -> S2SClientResult<Option<bp_messages::MessageData<u128>>> {
+        let expected_message_key = SmartCodecMapper::map_to(&message_key)?;
+        match self
+            .runtime()
+            .storage()
+            .bridge_pangolin_parachain_messages()
+            .outbound_messages(expected_message_key, hash)
+            .await?
+        {
+            Some(v) => Ok(Some(SmartCodecMapper::map_to(&v)?)),
+            None => Ok(None),
+        }
+    }
+
+    async fn read_proof(
+        &self,
+        storage_keys: Vec<sp_core::storage::StorageKey>,
+        hash: Option<<Self::Config as Config>::Hash>,
+    ) -> S2SClientResult<Vec<Vec<u8>>> {
+        let read_proof = self.subxt().rpc().read_proof(storage_keys, hash).await?;
+        let proof: Vec<Vec<u8>> = read_proof.proof.into_iter().map(|item| item.0).collect();
+        Ok(proof)
+    }
+
+    async fn receive_messages_proof(
+        &self,
+        relayer_id_at_bridged_chain: AccountId32,
+        proof: bridge_runtime_common::messages::target::FromBridgedChainMessagesProof<
+            <Self::Config as Config>::Hash,
+        >,
+        messages_count: u32,
+        dispatch_weight: u64,
+    ) -> S2SClientResult<<Self::Config as Config>::Hash> {
+        let expected_proof = SmartCodecMapper::map_to(&proof)?;
+        Ok(self
+            .runtime()
+            .tx()
+            .bridge_pangolin_parachain_messages()
+            .receive_messages_proof(
+                relayer_id_at_bridged_chain,
+                expected_proof,
+                messages_count,
+                dispatch_weight,
+            )
+            .sign_and_submit(self.account().signer())
+            .await?)
+    }
+
+    async fn receive_messages_delivery_proof(
+        &self,
+        proof: bridge_runtime_common::messages::source::FromBridgedChainMessagesDeliveryProof<
+            <Self::Config as Config>::Hash,
+        >,
+        relayers_state: bp_messages::UnrewardedRelayersState,
+    ) -> S2SClientResult<<Self::Config as Config>::Hash> {
+        let expected_proof = SmartCodecMapper::map_to(&proof)?;
+        let expected_relayers_state = SmartCodecMapper::map_to(&relayers_state)?;
+        Ok(self
+            .runtime()
+            .tx()
+            .bridge_pangolin_parachain_messages()
+            .receive_messages_delivery_proof(expected_proof, expected_relayers_state)
+            .sign_and_submit(self.account().signer())
+            .await?)
+    }
+
+    async fn best_para_heads(
+        &self,
+        para_id: crate::types::ParaId,
+        hash: Option<<Self::Config as Config>::Hash>,
+    ) -> S2SClientResult<Option<crate::types::BestParaHead>> {
+        Ok(self
+            .runtime()
+            .storage()
+            .bridge_rococo_parachains()
+            .best_para_heads(para_id, hash)
+            .await?)
+    }
+
+    async fn submit_parachain_heads(
+        &self,
+        relay_block_hash: <Self::Config as Config>::Hash,
+        parachains: Vec<crate::types::ParaId>,
+        parachain_heads_proof: Vec<Vec<u8>>,
+    ) -> S2SClientResult<<Self::Config as Config>::Hash> {
+        Ok(self
+            .runtime()
+            .tx()
+            .bridge_rococo_parachains()
+            .submit_parachain_heads(
+                relay_block_hash,
+                parachains,
+                parachain_heads_proof
+            )
+            .sign_and_submit(self.account().signer())
+            .await?)
+    }
+}
--- frame/assistants/client-pangolin/src/fastapi/bridge_s2s/mod.rs
+++ frame/assistants/client-pangolin/src/fastapi/bridge_s2s/mod.rs
@@ -1,3 +1,5 @@
 #[cfg(feature = "bridge-s2s-pangoro")]
 pub mod bridge_pangoro;
+#[cfg(feature = "bridge-s2s-pangolinparachain")]
+pub mod bridge_pangolinparachain;
 pub mod generic;

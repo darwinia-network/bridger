5c3ec6573f97f6def9e319bbcc2078b28899c4b0
fewensa <fewensa@protonmail.com>
2022-07-01 15:25:52 +0800

    improve feature

--- frame/assistants/client-pangolin/src/types/mod.rs
+++ frame/assistants/client-pangolin/src/types/mod.rs
@@ -1,5 +1,7 @@
 // expose raw client runtime types
-pub use crate::subxt_runtime::{api::runtime_types, EthereumReceiptProofThing};
+pub use crate::subxt_runtime::api::runtime_types;
+#[cfg(feature = "ethlike-v1")]
+pub use crate::subxt_runtime::EthereumReceiptProofThing;
 
 pub use self::account::*;
 pub use self::custom::*;

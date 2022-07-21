5ad886c74006e9a02ab073030440009207767a80
nada <37610161+furoxr@users.noreply.github.com>
2022-07-21 15:07:33 +0800

    Build message storage proof

--- bridges/pangoro-kiln/bridge/src/kiln_client/message_client.rs
+++ bridges/pangoro-kiln/bridge/src/kiln_client/message_client.rs
@@ -1,8 +1,8 @@
+use support_common::error::BridgerError;
 use web3::{
-    contract::Contract,
-    ethabi::{encode, token::Tokenizer},
+    signing::keccak256,
     transports::Http,
-    types::{Address, BlockNumber, Proof as Web3Proof, H256, U256},
+    types::{Address, BlockNumber, Bytes, Proof as Web3Proof, U256},
     Web3,
 };
 
@@ -35,7 +35,7 @@ impl KilnMessageClient {
         })
     }
 
-    pub async fn get_messages_proof(
+    pub async fn build_messages_proof(
         &self,
         begin: u64,
         end: u64,
@@ -47,15 +47,56 @@ impl KilnMessageClient {
                 vec![U256::from(LANE_IDENTIFY_SLOT)],
                 block_number,
             )
-            .await?;
+            .await?
+            .ok_or_else(|| BridgerError::Custom("Failed to get lane_id_proof".into()))?;
         let lane_nonce_proof = self
             .get_storage_proof(
                 self.outbound.contract.address(),
                 vec![U256::from(LANE_NONCE_SLOT)],
                 block_number,
             )
-            .await?;
+            .await?
+            .ok_or_else(|| BridgerError::Custom("Failed to get lane_nonce_proof".into()))?;
+        let message_keys = Self::build_message_storage_keys(begin, end);
+        let message_proof = self
+            .get_storage_proof(self.outbound.contract.address(), message_keys, block_number)
+            .await?
+            .ok_or_else(|| BridgerError::Custom("Failed to get message_proof".into()))?;
+
+        let account_proof = Self::encode_proof(&lane_id_proof.account_proof);
+        let lane_id_proof_ = Self::encode_proof(&lane_id_proof.storage_proof[0].proof);
+        let lane_nonce_proof_ = Self::encode_proof(&lane_nonce_proof.storage_proof[0].proof);
+        let message_proof = message_proof
+            .storage_proof
+            .iter()
+            .map(|x| Self::encode_proof(&x.proof));
+
         Ok(())
+    }
+
+    fn encode_proof(proofs: &Vec<Bytes>) -> Bytes {
+        Bytes::from(
+            &rlp::encode_list::<Vec<u8>, _>(
+                proofs
+                    .iter()
+                    .map(|x| x.0.clone())
+                    .collect::<Vec<Vec<u8>>>()
+                    .as_slice(),
+            )[..],
+        )
+    }
+
+    pub fn build_message_storage_keys(begin: u64, end: u64) -> Vec<U256> {
+        (begin..=end)
+            .map(|pos| {
+                let pos = U256::from(pos);
+                let slot = U256::from(LANE_MESSAGE_SLOT);
+                let bytes: &mut [u8] = &mut [0u8; 64];
+                pos.to_big_endian(&mut bytes[..32]);
+                slot.to_big_endian(&mut bytes[32..]);
+                U256::from(keccak256(bytes))
+            })
+            .collect()
     }
 
     pub async fn get_storage_proof(
@@ -69,5 +110,35 @@ impl KilnMessageClient {
             .eth()
             .proof(address, storage_keys, block_number)
             .await?)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn test_client() -> KilnMessageClient {
+        KilnMessageClient::new(
+            "http://localhost:8545",
+            "0xE04c799682F9509CF3D23A15F4A8ddc32648EDd4",
+            "0x4214611Be6cA4E337b37e192abF076F715Af4CaE",
+        )
+        .unwrap()
+    }
+
+    #[ignore]
+    #[tokio::test]
+    async fn test_get_storage_proof() {
+        let client = test_client();
+        let (begin, end) = (1, 2);
+        let message_keys = KilnMessageClient::build_message_storage_keys(begin, end);
+        println!("Message keys: {:?}", message_keys);
+        let message_proof = client
+            .get_storage_proof(client.outbound.contract.address(), message_keys, None)
+            .await
+            .unwrap()
+            .ok_or_else(|| BridgerError::Custom("Failed to get message_proof".into()))
+            .unwrap();
+        println!("Proof: {:?}", message_proof);
     }
 }

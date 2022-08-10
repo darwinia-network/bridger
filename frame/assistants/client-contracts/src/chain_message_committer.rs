3fd33b4b181a0b18cc9afd09b12797e85da42935
nada <37610161+furoxr@users.noreply.github.com>
2022-08-10 14:19:44 +0800

    Add prove method

--- frame/assistants/client-contracts/src/chain_message_committer.rs
+++ frame/assistants/client-contracts/src/chain_message_committer.rs
@@ -1,11 +1,13 @@
 use web3::{
     contract::{Contract, Options},
     transports::Http,
-    types::{Address, H256},
+    types::{Address, H256, U256},
     Web3,
 };
 
 use crate::error::BridgeContractResult;
+
+use self::types::MessageProof;
 
 pub struct ChainMessageCommitter {
     pub contract: Contract<Http>,
@@ -27,6 +29,92 @@ impl ChainMessageCommitter {
             .contract
             .query("commitment", (), None, Options::default(), None)
             .await?)
+    }
+
+    pub async fn prove(
+        &self,
+        chain_pos: U256,
+        lane_pos: U256,
+    ) -> BridgeContractResult<MessageProof> {
+        Ok(self
+            .contract
+            .query(
+                "prove",
+                (chain_pos, lane_pos),
+                None,
+                Options::default(),
+                None,
+            )
+            .await?)
+    }
+}
+
+pub mod types {
+    use web3::{
+        contract::tokens::{Detokenize, Tokenizable, TokenizableItem},
+        ethabi::Token,
+        types::H256,
+    };
+
+    #[derive(Debug, Clone)]
+    pub struct MessageProof {
+        pub chain_proof: MessageSingleProof,
+        pub lane_proof: MessageSingleProof,
+    }
+
+    impl Tokenizable for MessageProof {
+        fn from_token(token: Token) -> Result<Self, web3::contract::Error>
+        where
+            Self: Sized,
+        {
+            if let Token::Tuple(tokens) = token {
+                let (chain_proof, lane_proof) = Detokenize::from_tokens(tokens)?;
+                Ok(Self {
+                    chain_proof,
+                    lane_proof,
+                })
+            } else {
+                Err(web3::contract::Error::InvalidOutputType(format!(
+                    "Failed to decode from : {:?}",
+                    token
+                )))
+            }
+        }
+
+        fn into_token(self) -> Token {
+            Token::Tuple(vec![
+                self.chain_proof.into_token(),
+                self.lane_proof.into_token(),
+            ])
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct MessageSingleProof {
+        pub root: H256,
+        pub proof: Vec<H256>,
+    }
+
+    impl TokenizableItem for MessageSingleProof {}
+    impl Tokenizable for MessageSingleProof {
+        fn from_token(token: web3::ethabi::Token) -> Result<Self, web3::contract::Error>
+        where
+            Self: Sized,
+        {
+            if let Token::Tuple(tokens) = token {
+                let (root, proof) = Detokenize::from_tokens(tokens)?;
+                Ok(Self { root, proof })
+            } else {
+                Err(web3::contract::Error::InvalidOutputType(format!(
+                    "Failed to decode from : {:?}",
+                    token
+                )))
+            }
+        }
+
+        fn into_token(self) -> web3::ethabi::Token {
+            Token::Tuple(vec![self.root.into_token(), self.proof.into_token()])
+        }
     }
 }
 

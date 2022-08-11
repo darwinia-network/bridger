a3daf59d9460e2aca43566ffe3abbcfce1c94c82
nada <37610161+furoxr@users.noreply.github.com>
2022-08-11 16:56:50 +0800

    Add darwinia message client

---
+++ bridges/pangoro-kiln/bridge/src/message_contract/darwinia_message_client.rs
@@ -0,0 +1,97 @@
+use std::str::FromStr;
+
+use bridge_e2e_traits::strategy::RelayStrategy;
+
+use secp256k1::SecretKey;
+use support_common::error::BridgerError;
+use web3::{
+    contract::tokens::Tokenizable,
+    ethabi::encode,
+    transports::Http,
+    types::{Address, BlockId, BlockNumber, Bytes},
+    Web3,
+};
+
+use client_contracts::{
+    inbound_types::ReceiveMessagesProof, ChainMessageCommitter, Inbound, LaneMessageCommitter,
+};
+use client_contracts::{Outbound, SimpleFeeMarket};
+
+use super::{
+    simple_fee_market::SimpleFeeMarketRelayStrategy,
+    utils::{build_darwinia_delivery_proof, build_messages_data},
+};
+
+pub struct DarwiniaMessageClient<T: RelayStrategy> {
+    pub client: Web3<Http>,
+    pub inbound: Inbound,
+    pub outbound: Outbound,
+    pub chain_message_committer: ChainMessageCommitter,
+    pub lane_message_committer: LaneMessageCommitter,
+    pub strategy: T,
+    pub private_key: Option<SecretKey>,
+}
+
+pub fn build_darwinia_message_client(
+    endpoint: &str,
+    inbound_address: Address,
+    outbound_address: Address,
+    chain_message_committer_address: Address,
+    lane_message_committer_address: Address,
+    fee_market_address: Address,
+    account: Address,
+    private_key: Option<&str>,
+) -> color_eyre::Result<DarwiniaMessageClient<SimpleFeeMarketRelayStrategy>> {
+    let transport = Http::new(endpoint)?;
+    let client = Web3::new(transport);
+    let inbound = Inbound::new(&client, inbound_address)?;
+    let outbound = Outbound::new(&client, outbound_address)?;
+    let fee_market = SimpleFeeMarket::new(&client, fee_market_address)?;
+    let strategy = SimpleFeeMarketRelayStrategy::new(fee_market, account);
+    let chain_message_committer =
+        ChainMessageCommitter::new(&client, chain_message_committer_address)?;
+    let lane_message_committer =
+        LaneMessageCommitter::new(&client, lane_message_committer_address)?;
+    let private_key = private_key.map(SecretKey::from_str).transpose()?;
+    Ok(DarwiniaMessageClient {
+        client,
+        inbound,
+        outbound,
+        chain_message_committer,
+        lane_message_committer,
+        strategy,
+        private_key,
+    })
+}
+
+impl<T: RelayStrategy> DarwiniaMessageClient<T> {
+    pub fn private_key(&self) -> color_eyre::Result<SecretKey> {
+        Ok(self
+            .private_key
+            .ok_or_else(|| BridgerError::Custom("Private key not found!".into()))?)
+    }
+
+    pub async fn prepare_for_messages_delivery(
+        &self,
+        begin: u64,
+        end: u64,
+        block_number: Option<BlockNumber>,
+    ) -> color_eyre::Result<ReceiveMessagesProof> {
+        let outbound_lane_data =
+            build_messages_data(&self.client, &self.outbound, begin, end).await?;
+        let proof = build_darwinia_delivery_proof(
+            &self.outbound,
+            &self.lane_message_committer,
+            &self.chain_message_committer,
+            block_number.map(BlockId::from),
+        )
+        .await?
+        .into_token();
+
+        let messages_proof = Bytes(encode(&[proof]));
+        Ok(ReceiveMessagesProof {
+            outbound_lane_data,
+            messages_proof,
+        })
+    }
+}
--- bridges/pangoro-kiln/bridge/src/message_contract/mod.rs
+++ bridges/pangoro-kiln/bridge/src/message_contract/mod.rs
@@ -1,2 +1,4 @@
+pub mod darwinia_message_client;
 pub mod message_client;
 pub mod simple_fee_market;
+pub mod utils;

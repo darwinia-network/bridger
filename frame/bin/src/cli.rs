1dd05b35630a8cdd22629f990c3d3f25c7720519
fewensa <37804932+fewensa@users.noreply.github.com>
2022-09-28 20:01:02 +0800

    Update rust docs (#520)

--- frame/bin/src/cli.rs
+++ frame/bin/src/cli.rs
@@ -2,11 +2,7 @@ use crate::command::handler;
 use crate::Opt;
 
 pub fn execute(opt: Opt) -> color_eyre::Result<()> {
-    if let Err(e) = run(opt) {
-        // maybe there have some special error to handle.
-        return Err(e);
-    }
-    Ok(())
+    run(opt)
 }
 
 fn run(opt: Opt) -> color_eyre::Result<()> {

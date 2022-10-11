3e8f5670f79a7f761cbd1f197ede639fa2bb6b1b
fewensa <fewensa@protonmail.com>
2022-10-11 16:09:54 +0800

    Clean ethv1 bridge

--- bridges/darwinia-ethereum-pow/bridge/src/bridge/bus.rs
+++
@@ -1,7 +0,0 @@
-use lifeline::prelude::*;
-
-use component_state::state::BridgeState;
-
-lifeline_bus!(pub struct DarwiniaEthereumBus);
-
-impl Resource<DarwiniaEthereumBus> for BridgeState {}
--- bridges/darwinia-ethereum-pow/bridge/src/command/handler/start.rs
+++
@@ -1,9 +0,0 @@
-use crate::bridge::DarwiniaEthereumTask;
-
-pub async fn handle_start() -> color_eyre::Result<()> {
-    tracing::info!("Start bridge darwinia-ropsten");
-    let _task = DarwiniaEthereumTask::new().await?;
-    loop {
-        tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-    }
-}

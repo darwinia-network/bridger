38a49d2c7e1dad79317631a145e3ceced117e4e7
nada <37610161+furoxr@users.noreply.github.com>
2022-10-25 20:09:28 +0800

    Replace sync committee relay

--- bridges/pangoro-goerli/bridge/src/service/header_relay/sync_committee_update.rs
+++ bridges/pangoro-goerli/bridge/src/service/header_relay/sync_committee_update.rs
@@ -1,17 +1,14 @@
-use std::{ops::Div, time::Duration};
-
 use crate::{
     bridge::{BridgeBus, BridgeConfig},
     pangoro_client::client::PangoroClient,
-    web3_helper::{wait_for_transaction_confirmation, GasPriceOracle},
 };
-use client_beacon::{client::BeaconApiClient, types::Proof};
-use client_contracts::beacon_light_client_types::SyncCommitteePeriodUpdate;
+use client_beacon::client::BeaconApiClient;
 use lifeline::{Lifeline, Service, Task};
+use relay_e2e::header::eth_sync_committee_relay::SyncCommitteeRelayRunner;
 use support_common::config::{Config, Names};
-use support_common::error::BridgerError;
+
 use support_lifeline::service::BridgeService;
-use web3::{contract::Options, types::U256};
+use web3::types::U256;
 
 #[derive(Debug)]
 pub struct SyncCommitteeUpdateService {
@@ -50,128 +47,12 @@ async fn start() -> color_eyre::Result<()> {
         U256::from_dec_str(&config.pangoro_evm.max_gas_price)?,
     )?;
     let goerli_client = BeaconApiClient::new(&config.goerli.endpoint)?;
-    let update_manager = SyncCommitteeUpdate {
-        pangoro_client,
-        goerli_client,
+    let mut update_manager = SyncCommitteeRelayRunner {
+        eth_light_client: pangoro_client,
+        beacon_api_client: goerli_client,
     };
 
-    loop {
-        if let Err(error) = update_manager.sync_committee_update().await {
-            tracing::error!(
-                target: "pangoro-goerli",
-                "[SyncCommittee][Goerli=>Pangoro] Failed relay sync committee update : {:?}",
-                error
-            );
-            return Err(error);
-        }
-        tokio::time::sleep(std::time::Duration::from_secs(60)).await;
-    }
-}
-
-pub struct SyncCommitteeUpdate {
-    pub pangoro_client: PangoroClient,
-    pub goerli_client: BeaconApiClient,
-}
-
-impl SyncCommitteeUpdate {
-    pub async fn sync_committee_update(&self) -> color_eyre::Result<()> {
-        let last_relayed_header = self
-            .pangoro_client
-            .beacon_light_client
-            .finalized_header()
-            .await?;
-        let period = last_relayed_header.slot.div(32).div(256);
-
-        let _current_sync_committee = self
-            .pangoro_client
-            .beacon_light_client
-            .sync_committee_roots(period)
-            .await?;
-        let next_sync_committee = self
-            .pangoro_client
-            .beacon_light_client
-            .sync_committee_roots(period + 1)
-            .await?;
-        if next_sync_committee.is_zero() {
-            tracing::info!(
-                target: "pangoro-goerli",
-                "[SyncCommittee][Goerli=>Pangoro] Try to relay SyncCommittee at period {:?}",
-                period + 1,
-            );
-
-            let sync_committee_update = self
-                .get_sync_committee_update_parameter(period, last_relayed_header.slot)
-                .await?;
-
-            let gas_price = self.pangoro_client.gas_price().await?;
-            let tx = self
-                .pangoro_client
-                .beacon_light_client
-                .import_next_sync_committee(
-                    sync_committee_update,
-                    &self.pangoro_client.private_key,
-                    Options {
-                        gas: Some(U256::from_dec_str("10000000")?),
-                        gas_price: Some(gas_price),
-                        ..Default::default()
-                    },
-                )
-                .await?;
-
-            tracing::info!(
-                target: "pangoro-goerli",
-                "[SyncCommittee][Goerli=>Pangoro] Sending tx: {:?}",
-                &tx
-            );
-            wait_for_transaction_confirmation(
-                tx,
-                self.pangoro_client.client.transport(),
-                Duration::from_secs(5),
-                3,
-            )
-            .await?;
-        } else {
-            tracing::info!(
-                target: "pangoro-goerli",
-                "[SyncCommittee][Goerli=>Pangoro] Next sync committee is {:?}",
-                next_sync_committee
-            );
-        }
-        Ok(())
-    }
-
-    async fn get_sync_committee_update_parameter(
-        &self,
-        period: u64,
-        slot: u64,
-    ) -> color_eyre::Result<SyncCommitteePeriodUpdate> {
-        let sync_committee_update = self
-            .goerli_client
-            .get_sync_committee_period_update(period, 1)
-            .await?;
-        if sync_committee_update.is_empty() {
-            return Err(BridgerError::Custom("Failed to get sync committee update".into()).into());
-        }
-        let next_sync_committee = sync_committee_update
-            .get(0)
-            .expect("Unreachable!")
-            .next_sync_committee
-            .clone();
-        let next_sync_committee_branch = self
-            .goerli_client
-            .get_next_sync_committee_branch(slot)
-            .await?;
-        let witnesses = match next_sync_committee_branch {
-            Proof::SingleProof {
-                gindex: _,
-                leaf: _,
-                witnesses,
-            } => witnesses,
-            _ => return Err(BridgerError::Custom("Not implemented!".to_string()).into()),
-        };
-        Ok(SyncCommitteePeriodUpdate {
-            sync_committee: next_sync_committee.to_contract_type()?,
-            next_sync_committee_branch: witnesses,
-        })
-    }
+    update_manager.start().await?;
+    tokio::time::sleep(std::time::Duration::from_secs(60)).await;
+    Ok(())
 }

edcc58474683fad8272f964c4a98551751ea278f
nada <37610161+furoxr@users.noreply.github.com>
2022-10-25 20:09:18 +0800

    Replace execution layer relay

--- bridges/pangoro-goerli/bridge/src/service/header_relay/execution_layer_update.rs
+++ bridges/pangoro-goerli/bridge/src/service/header_relay/execution_layer_update.rs
@@ -1,20 +1,14 @@
-use std::{str::FromStr, time::Duration};
-
 use crate::{
     bridge::{BridgeBus, BridgeConfig},
     pangoro_client::client::PangoroClient,
-    web3_helper::{wait_for_transaction_confirmation, GasPriceOracle},
 };
-use client_beacon::{client::BeaconApiClient, types::Proof};
+use client_beacon::client::BeaconApiClient;
 use lifeline::{Lifeline, Service, Task};
+use relay_e2e::header::eth_execution_layer_relay::ExecutionLayerRelayRunner;
 use support_common::config::{Config, Names};
-use support_common::error::BridgerError;
+
 use support_lifeline::service::BridgeService;
-use web3::{
-    contract::{tokens::Tokenize, Options},
-    ethabi::Token,
-    types::{H256, U256},
-};
+use web3::types::U256;
 
 #[derive(Debug)]
 pub struct ExecutionLayerRelay {
@@ -53,106 +47,11 @@ async fn start() -> color_eyre::Result<()> {
         U256::from_dec_str(&config.pangoro_evm.max_gas_price)?,
     )?;
     let goerli_client = BeaconApiClient::new(&config.goerli.endpoint)?;
-    let execution_layer_relay = ExecutionLayer {
-        pangoro_client,
-        goerli_client,
+    let mut execution_layer_relay = ExecutionLayerRelayRunner {
+        eth_light_client: pangoro_client,
+        beacon_api_client: goerli_client,
     };
 
-    loop {
-        if let Err(error) = execution_layer_relay.execution_layer_relay().await {
-            tracing::error!(
-                target: "pangoro-goerli",
-                "Failed to relay exection payload state root: {:?}",
-                error
-            );
-            return Err(error);
-        }
-        tokio::time::sleep(std::time::Duration::from_secs(15)).await;
-    }
-}
-
-pub struct ExecutionLayer {
-    pub pangoro_client: PangoroClient,
-    pub goerli_client: BeaconApiClient,
-}
-
-impl ExecutionLayer {
-    pub async fn execution_layer_relay(&self) -> color_eyre::Result<()> {
-        let last_relayed_header = self
-            .pangoro_client
-            .beacon_light_client
-            .finalized_header()
-            .await?;
-        let finalized_block = self
-            .goerli_client
-            .get_beacon_block(last_relayed_header.slot)
-            .await?;
-        let latest_execution_payload_state_root =
-            H256::from_str(&finalized_block.body.execution_payload.state_root)?;
-        let relayed_state_root = self
-            .pangoro_client
-            .execution_layer
-            .merkle_root(None)
-            .await?;
-
-        if relayed_state_root != latest_execution_payload_state_root {
-            tracing::info!(
-                target: "pangoro-goerli",
-                "[ExecutionLayer][Goerli=>Pangoro] Try to relay execution layer state at slot: {:?}",
-                last_relayed_header.slot,
-            );
-
-            let state_root_branch = self
-                .goerli_client
-                .get_latest_execution_payload_state_root_branch(last_relayed_header.slot)
-                .await?;
-            let witnesses = match state_root_branch {
-                Proof::SingleProof {
-                    gindex: _,
-                    leaf: _,
-                    witnesses,
-                } => witnesses,
-                _ => return Err(BridgerError::Custom("Not implemented!".to_string()).into()),
-            };
-            let parameter =
-                Token::Tuple((latest_execution_payload_state_root, witnesses).into_tokens());
-
-            let gas_price = self.pangoro_client.gas_price().await?;
-            let tx = self
-                .pangoro_client
-                .execution_layer
-                .contract
-                .signed_call(
-                    "import_latest_execution_payload_state_root",
-                    (parameter,),
-                    Options {
-                        gas: Some(U256::from(10000000)),
-                        gas_price: Some(gas_price),
-                        ..Default::default()
-                    },
-                    &self.pangoro_client.private_key,
-                )
-                .await?;
-            tracing::info!(
-                target: "pangoro-goerli",
-                "[ExecutionLayer][Goerli=>Pangoro] Sending tx: {:?}",
-                &tx
-            );
-            wait_for_transaction_confirmation(
-                tx,
-                self.pangoro_client.client.transport(),
-                Duration::from_secs(5),
-                3,
-            )
-            .await?;
-        } else {
-            tracing::info!(
-                target: "pangoro-goerli",
-                "[ExecutionLayer][Goerli=>Pangoro] Latest execution payload state root at slot {:?} is : {:?}",
-                last_relayed_header.slot,
-                &relayed_state_root,
-            );
-        }
-        Ok(())
-    }
+    execution_layer_relay.start().await?;
+    Ok(())
 }

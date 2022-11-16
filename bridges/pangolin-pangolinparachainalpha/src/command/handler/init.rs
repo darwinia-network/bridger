d185b6899ec45d1050ee1b859e07a86d10f18e73
fewensa <fewensa@protonmail.com>
2022-11-16 20:16:47 +0800

    migrate pangolin-pangolinparachainalpha

---
+++ bridges/pangolin-pangolinparachainalpha/src/command/handler/init.rs
@@ -0,0 +1,41 @@
+use bridge_s2s_traits::client::{S2SClientGeneric, S2SClientRelay};
+
+use bin_s2s::traits::{
+    S2SParaBridgeRelayChainInfo, S2SParaBridgeSoloChainInfo, S2SSoloBridgeSoloChainInfo,
+};
+use support_common::config::{Config, Names};
+use support_toolkit::convert::SmartCodecMapper;
+
+use crate::types::{BridgeFlow, RawBridgeConfig};
+
+pub async fn handle_init(bridge: BridgeFlow) -> color_eyre::Result<()> {
+    tracing::info!(target: "pangolin-pangolinparachain", "init bridge {:?}", bridge);
+    let bridge_config: RawBridgeConfig =
+        Config::restore(Names::BridgePangolinPangolinParachainAlpha)?;
+    init_bridge(bridge, bridge_config).await?;
+    Ok(())
+}
+
+async fn init_bridge(bridge: BridgeFlow, bridge_config: RawBridgeConfig) -> color_eyre::Result<()> {
+    let client_pangolin = bridge_config.pangolin.client().await?;
+    let client_moonbase = bridge_config.moonbase.client().await?;
+    let client_pangolin_parachain = bridge_config.pangolin_parachain_alpha.client().await?;
+    let hash = match bridge {
+        BridgeFlow::PangolinToPangolinParachainAlpha => {
+            let initialization_data = client_pangolin.prepare_initialization_data().await?;
+            let expected_data = SmartCodecMapper::map_to(&initialization_data)?;
+            client_pangolin_parachain.initialize(expected_data).await?
+        }
+        BridgeFlow::MoonbaseToPangolin => {
+            let initialization_data = client_moonbase.prepare_initialization_data().await?;
+            let expected_data = SmartCodecMapper::map_to(&initialization_data)?;
+            client_pangolin.initialize(expected_data).await?
+        }
+    };
+    tracing::info!(
+        target: "pangolin-pangolinparachain",
+        "successes to sent init transaction: {:?}",
+        hash,
+    );
+    Ok(())
+}
---
+++ bridges/pangolin-pangolinparachainalpha/src/command/types.rs
@@ -0,0 +1,20 @@
+use structopt::StructOpt;
+
+use crate::types::BridgeFlow;
+
+/// Bridge pangolin-pangolinparachain operations
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "pangolin-pangolinparachain",
+    about = "Bridge pangolin-pangolinparachain"
+)]
+pub enum Opts {
+    /// Init bridge
+    Init {
+        /// Bridge flow direction, [moonbase-to-pangolin | pangolin-to-pangolin-parachain]
+        #[structopt()]
+        bridge: BridgeFlow,
+    },
+    /// Start bridge
+    Start,
+}

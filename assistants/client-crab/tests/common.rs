1623c3cf8399c6cf665892a81035f45fb8bd60fc
fewensa <osuni@protonmail.com>
2022-12-22 14:16:24 +0800

    add unit test

--- assistants/client-crab/tests/common.rs
+++ assistants/client-crab/tests/common.rs
@@ -5,7 +5,7 @@ use client_crab::error::ClientResult;
 
 pub async fn client() -> ClientResult<CrabClient> {
     let config = ClientConfig {
-        endpoint: "wss://crab-rpc.darwinia.network".to_string(),
+        endpoint: "ws://g2.dev.darwinia.network:2244".to_string(),
         relayer_private_key: "//Alice".to_string(),
         relayer_real_account: None,
     };
--- assistants/client-crab/tests/test_component.rs
+++ assistants/client-crab/tests/test_component.rs
@@ -1,4 +1,10 @@
+use client_crab::types::DarwiniaAccount;
+use support_toolkit::convert::SmartCodecMapper;
+
 mod common;
+
+const ALICE: &str = "0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133";
+const BALTATHAR: &str = "0x8075991ce870b93a8870eca0c0f91913d12f47948ca0fd25b49c6fa7cdbeee8b";
 
 #[tokio::test]
 async fn test_client() {
@@ -30,11 +36,11 @@ async fn test_spec_version() {
 #[tokio::test]
 async fn test_system_number() {
     let client = common::client().await.unwrap();
+    let address = client_crab::runtime_api::storage().system().number();
     let number = client
-        .runtime()
+        .subxt()
         .storage()
-        .system()
-        .number(None)
+        .fetch_or_default(&address, None)
         .await
         .unwrap();
     println!("{:?}", number);
@@ -43,37 +49,49 @@ async fn test_system_number() {
 #[tokio::test]
 async fn test_technical_committee_members() {
     let client = common::client().await.unwrap();
-    let members = client
-        .runtime()
-        .storage()
+    let address = client_crab::runtime_api::storage()
         .technical_committee()
-        .members(None)
+        .members();
+    let members = client
+        .subxt()
+        .storage()
+        .fetch_raw(&address.to_bytes(), None)
         .await
         .unwrap();
     println!("{:?}", members);
 }
 
 #[tokio::test]
-async fn authorities_to_sign() {
+async fn test_transfer_call() {
     let client = common::client().await.unwrap();
-    let atd = client
-        .runtime()
-        .storage()
-        .ethereum_relay_authorities()
-        .authorities_to_sign(None)
+    let source = DarwiniaAccount::new(ALICE.to_string(), None).unwrap();
+    let dest = DarwiniaAccount::new(BALTATHAR.to_string(), None).unwrap();
+    let call = client_crab::runtime_api::tx().balances().transfer(
+        SmartCodecMapper::map_to(dest.account_id()).unwrap(),
+        100 * 10u128.pow(18),
+    );
+    let tx = client.subxt().tx().call_data(&call).unwrap();
+    println!("{:?}", array_bytes::bytes2hex("0x", tx));
+    let track = client
+        .subxt()
+        .tx()
+        .sign_and_submit_then_watch_default(&call, source.signer())
         .await
         .unwrap();
-    println!("{:?}", atd);
+    let events = track.wait_for_finalized_success().await.unwrap();
+    println!("{}", events.extrinsic_index());
+    println!("{}", array_bytes::bytes2hex("0x", events.extrinsic_hash()));
 }
 
 #[tokio::test]
+#[cfg(feature = "bridge-s2s")]
 async fn test_subscribe() {
+    use bridge_s2s_traits::client::S2SClientGeneric;
     use client_crab::types::runtime_types::{
         bp_header_chain::justification::GrandpaJustification, sp_runtime::generic::header::Header,
     };
     use codec::Decode;
-    use futures_util::stream::StreamExt;
-    use subxt::sp_runtime::traits::BlakeTwo256;
+    use sp_runtime::traits::BlakeTwo256;
 
     let client = common::client().await.unwrap();
     let mut it = client

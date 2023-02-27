58aea8ba2a3170387d4fb9cdec03aa8d18c76b23
fewensa <osuni@protonmail.com>
2023-02-27 07:59:41 +0000

    Bridge pangolin-pangoro

--- bridges/pangolin-pangoro/src/main.rs
+++ bridges/pangolin-pangoro/src/main.rs
@@ -1,4 +1,15 @@
+use structopt::StructOpt;
 
-fn main() {
+use crate::command::types::Opts;
 
+mod cli;
+mod command;
+mod types;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    support_common::initialize::init()?;
+    let opt = Opts::from_args();
+    cli::execute(opt).await?;
+    Ok(())
 }

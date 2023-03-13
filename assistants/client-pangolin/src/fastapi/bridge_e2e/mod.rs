8290c774c58f401a376cb4a9e9fa3460449f27b6
nada <37610161+furoxr@users.noreply.github.com>
2023-03-13 14:28:43 +0800

    feat:pangolin support bridge e2e

---
+++ assistants/client-pangolin/src/fastapi/bridge_e2e/mod.rs
@@ -0,0 +1,93 @@
+use crate::client::PangolinClient;
+use crate::config::PangolinSubxtConfig;
+use crate::types::runtime_types;
+use bridge_e2e_traits::{
+    client::EcdsaClient,
+    error::{E2EClientError, E2EClientResult},
+};
+
+#[async_trait::async_trait]
+impl EcdsaClient for PangolinClient {
+    type SubxtConfig = PangolinSubxtConfig;
+
+    /// current account is ecdsa authority account
+    async fn is_ecdsa_authority(
+        &self,
+        block_number: Option<u32>,
+        your_address: &[u8; 20],
+    ) -> E2EClientResult<bool> {
+        let hash = self
+            .subxt()
+            .rpc()
+            .block_hash(block_number.map(Into::into))
+            .await?;
+        let authorities = crate::subxt_runtime::api::storage()
+            .ecdsa_authority()
+            .authorities();
+        let authorities = self.subxt().storage().fetch(&authorities, hash).await?;
+        match authorities {
+            None => Ok(false),
+            Some(authorities) => {
+                let authorities = authorities.0;
+                let iam = authorities.iter().any(|item| {
+                    let authority: &[u8; 20] = &item.0;
+                    authority == your_address
+                });
+                Ok(iam)
+            }
+        }
+    }
+
+    async fn submit_authorities_change_signature(
+        &self,
+        signatures: Vec<u8>,
+    ) -> E2EClientResult<<PangolinSubxtConfig as subxt::Config>::Hash> {
+        let fixed_signatures: [u8; 65] = signatures.try_into().map_err(|e: Vec<u8>| {
+            E2EClientError::Custom(format!(
+                "Wrong signatures data: {}",
+                array_bytes::bytes2hex("0x", e.as_slice())
+            ))
+        })?;
+        let tx = crate::subxt_runtime::api::tx()
+            .ecdsa_authority()
+            .submit_authorities_change_signature(runtime_types::sp_core::ecdsa::Signature(
+                fixed_signatures,
+            ));
+        let track = self
+            .subxt()
+            .tx()
+            .sign_and_submit_then_watch(&tx, self.account().signer(), Default::default())
+            .await?;
+        let events = track.wait_for_finalized_success().await.map_err(|e| {
+            E2EClientError::Custom(format!("send transaction failed pangolin: {:?}", e))
+        })?;
+        Ok(events.extrinsic_hash())
+    }
+
+    async fn submit_new_message_root_signature(
+        &self,
+        signatures: Vec<u8>,
+    ) -> E2EClientResult<<PangolinSubxtConfig as subxt::Config>::Hash> {
+        let fixed_signatures: [u8; 65] = signatures.try_into().map_err(|e: Vec<u8>| {
+            E2EClientError::Custom(format!(
+                "Wrong signatures data: {}",
+                array_bytes::bytes2hex("0x", e.as_slice())
+            ))
+        })?;
+        let tx = crate::subxt_runtime::api::tx()
+            .ecdsa_authority()
+            .submit_new_message_root_signature(runtime_types::sp_core::ecdsa::Signature(
+                fixed_signatures,
+            ));
+
+        let track = self
+            .subxt()
+            .tx()
+            .sign_and_submit_then_watch(&tx, self.account().signer(), Default::default())
+            .await?;
+        let events = track.wait_for_finalized_success().await.map_err(|e| {
+            E2EClientError::Custom(format!("send transaction failed pangolin: {:?}", e))
+        })?;
+        Ok(events.extrinsic_hash())
+    }
+}
--- assistants/client-pangolin/src/fastapi/mod.rs
+++ assistants/client-pangolin/src/fastapi/mod.rs
@@ -3,3 +3,5 @@ pub mod bridge_s2s;
 mod common;
 #[cfg(feature = "feemarket-s2s")]
 pub mod feemarket_s2s;
+#[cfg(feature = "bridge-e2e")]
+pub mod bridge_e2e;
